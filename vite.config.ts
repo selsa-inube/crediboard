64ead06848d154c0fd5122d41a44212f7b311d88
carin <ca.rincon97@gmail.com>
2024-04-03 05:46:53 -0500

    feat: configuring aliases

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -7,5 +8,17 @@ export default defineConfig({
   server: {
     open: true,
     port: 3000,
+  },
+  resolve: {
+    alias: {
+      "@design": path.resolve(__dirname, "./src/design"),
+      "@components": path.resolve(__dirname, "./src/components"),
+      "@config": path.resolve(__dirname, "./src/config"),
+      "@pages": path.resolve(__dirname, "./src/pages"),
+      "@hooks": path.resolve(__dirname, "./src/hooks"),
+      "@mocks": path.resolve(__dirname, "./src/mocks"),
+      "@ptypes": path.resolve(__dirname, "./src/types"),
+      "@forms": path.resolve(__dirname, "./src/forms"),
+    },
   },
 });
