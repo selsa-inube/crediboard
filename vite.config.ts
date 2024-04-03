96ed7a9f2b01edaa448df56d1274c9b22f38fe01
carin <ca.rincon97@gmail.com>
2024-04-03 10:02:06 -0500

    fix: including lint-staged and vitesCondigPaths

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import vitesconfigPaths from "vite-tsconfig-paths";
+
 import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), vitesconfigPaths()],
   server: {
     open: true,
     port: 3000,
