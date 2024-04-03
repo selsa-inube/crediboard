7392d350c6b6821fb48952df4327d9e8f58a847f
carin <ca.rincon97@gmail.com>
2024-04-03 14:39:23 -0500

    fix: adjusting provider in auth

--- src/config/environment.ts
+++ src/config/environment.ts
@@ -5,7 +5,7 @@ const enviroment = {
   CLIENT_ID: import.meta.env.VITE_AUTH_CLIENT_ID,
   CLIENT_SECRET: import.meta.env.VITE_AUTH_CLIENT_SECRET,
   REALM: import.meta.env.VITE_AUTH_REALM,
-  PROVIDER: import.meta.env.VITE_AUTH_PROVIDER || "identidad",
+  PROVIDER: IS_PRODUCTION ? "identidad" : import.meta.env.VITE_AUTH_PROVIDER,
   REDIRECT_URI: IS_PRODUCTION ? window.location.origin : AUTH_REDIRECT_URI,
   ICLIENT_API_URL_QUERY: import.meta.env.VITE_ICLIENT_API_URL_QUERY,
   TEMP_BUSINESS_UNIT: "test",
