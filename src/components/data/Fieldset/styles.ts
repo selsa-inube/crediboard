be324242a4cc3d63cad5cc57802cb9c90615ace9
Andres Babativa <carlos3k11@gmail.com>
2024-04-10 09:24:59 -0500

    style(Fieldset): to use theme color for border-color

--- src/components/data/Fieldset/styles.ts
+++ src/components/data/Fieldset/styles.ts
@@ -3,7 +3,9 @@ import styled from "styled-components";
 import { inube } from "@inube/design-system";
 
 export const StyledContainerFieldset = styled.div`
-  border-color: ${inube.color.stroke.divider.regular};
+  border-color: ${({ theme }) =>
+    theme?.color?.stroke?.divider?.regular ||
+    inube.color.stroke.divider.regular};
   border-radius: 15px;
   border-width: 4px;
   border-style: solid;
