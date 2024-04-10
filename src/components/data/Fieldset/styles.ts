eae8a469f4e98029fa770316204b8d45c35a3e27
Andres Babativa <carlos3k11@gmail.com>
2024-04-10 10:07:34 -0500

    style(Fieldset): use color for border-color and add box-shadow

--- src/components/data/Fieldset/styles.ts
+++ src/components/data/Fieldset/styles.ts
@@ -1,13 +1,16 @@
 import styled from "styled-components";
-
 import { inube } from "@inube/design-system";
 
 export const StyledContainerFieldset = styled.div`
+  border-radius: 15px;
+  border-width: 2px;
+  border-style: solid;
   border-color: ${({ theme }) =>
     theme?.color?.stroke?.divider?.regular ||
     inube.color.stroke.divider.regular};
-  border-radius: 15px;
-  border-width: 4px;
-  border-style: solid;
+  box-shadow: 2px 2px 6px
+    ${({ theme }) =>
+      theme?.color?.stroke?.divider?.regular ||
+      inube.color.stroke.divider.regular};
   padding: ${({ theme }) => theme?.spacing?.s200 || inube.spacing.s200};
 `;
