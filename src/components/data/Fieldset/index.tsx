2f490c49468b6c89a12143530ecb5e6591d0b796
Andres Babativa <carlos3k11@gmail.com>
2024-04-10 09:36:59 -0500

    refactor(Fieldset): remove direction prop

--- src/components/data/Fieldset/index.tsx
+++ src/components/data/Fieldset/index.tsx
@@ -6,11 +6,10 @@ interface IFieldsetProps {
   title: string;
   descriptionTitle?: string;
   children: JSX.Element | JSX.Element[];
-  direction?: "row" | "column";
 }
 
 export const Fieldset = (props: IFieldsetProps) => {
-  const { children, title, descriptionTitle, direction = "row" } = props;
+  const { children, title, descriptionTitle } = props;
 
   return (
     <Stack direction="column" gap="8px">
@@ -23,7 +22,7 @@ export const Fieldset = (props: IFieldsetProps) => {
         </Text>
       </Stack>
       <StyledContainerFieldset>
-        <Stack direction={direction}>{children}</Stack>
+        <Stack>{children}</Stack>
       </StyledContainerFieldset>
     </Stack>
   );
