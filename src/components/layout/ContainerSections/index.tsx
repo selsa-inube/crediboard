de40d733d18175e33e95c8752e87aba3b3d86f5d
Andres Babativa <carlos3k11@gmail.com>
2024-04-11 11:45:46 -0500

    feat(ContainerSections): adjust spacing and import inube from @inube/design-system

--- src/components/layout/ContainerSections/index.tsx
+++ src/components/layout/ContainerSections/index.tsx
@@ -1,6 +1,6 @@
 import { MdArrowBack } from "react-icons/md";
 
-import { Button, Stack } from "@inube/design-system";
+import { Button, Stack, inube } from "@inube/design-system";
 import { StyledHorizontalRule } from "./styles";
 import { configButtons } from "./config";
 
@@ -18,14 +18,14 @@ export const ContainerSections = (props: IContainerSectionsProps) => {
             Volver
           </Button>
         </Stack>
-        <Stack justifyContent="end" gap="32px">
-          <Stack gap="16px">
+        <Stack justifyContent="end" gap={inube.spacing.s200}>
+          <Stack gap={inube.spacing.s400}>
             <Button>{configButtons.buttons.buttonOne.label}</Button>
             <Button>{configButtons.buttons.buttonTwo.label}</Button>
             <Button>{configButtons.buttons.buttonTree.label}</Button>
           </Stack>
           <StyledHorizontalRule />
-          <Stack gap="16px">
+          <Stack gap={inube.spacing.s200}>
             <Button variant="outlined">
               {configButtons.buttonsOutlined.buttonOne.label}
             </Button>
