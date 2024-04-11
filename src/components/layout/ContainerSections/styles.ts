b28de6abf218e3078e9051c85de1b1ea066c6565
Andres Babativa <carlos3k11@gmail.com>
2024-04-11 12:12:04 -0500

    style(ContainerSections): implement inube tokens

--- src/components/layout/ContainerSections/styles.ts
+++ src/components/layout/ContainerSections/styles.ts
@@ -1,9 +1,10 @@
 import styled from "styled-components";
 import { inube } from "@inube/design-system";
 
-export const StyledHorizontalRule = styled.hr`
-  margin: 0px;
-  width: 1px;
+export const StyledHorizontalDivider = styled.hr`
+  margin: ${inube.spacing.s0};
+  width: 2px;
+  border: none;
   background-color: ${({ theme }) =>
     theme?.color?.stroke?.divider?.regular ||
     inube.color.stroke.divider.regular};
