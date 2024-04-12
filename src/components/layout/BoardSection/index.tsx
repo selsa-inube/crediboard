a12d8cf91ddf4d840047ccd9e9d89f40831975a2
Johan Daniel Garcia Nova <gtahghfsg@gmail.com>
2024-04-12 18:54:00 -0500

    refactor(BoardSection): configure component with horizontal and  collapse options

--- src/components/layout/BoardSection/index.tsx
+++ src/components/layout/BoardSection/index.tsx
@@ -1,12 +1,15 @@
-import { Stack, Text, inube } from "@inube/design-system";
+import { useState } from "react";
+import { Stack, Text, Icon, useMediaQuery, inube } from "@inube/design-system";
+import { MdOutlineChevronRight } from "react-icons/md";
 
-import { StyledBoardSection, StyledDivider } from "./styles";
-import { SectionBackground } from "./types";
+import { StyledBoardSection, StyledCollapseIcon } from "./styles";
+import { SectionBackground, SectionOrientation } from "./types";
 
 interface IBoardSectionProps {
   sectionTitle: string;
   numberActiveCards: number;
   sectionBackground: SectionBackground;
+  orientation: SectionOrientation;
   children: JSX.Element | JSX.Element[];
 }
 
@@ -15,25 +18,68 @@ function BoardSection(props: IBoardSectionProps) {
     sectionTitle,
     numberActiveCards,
     sectionBackground = "light",
+    orientation = "vertical",
     children,
   } = props;
+
+  const smallScreen = useMediaQuery("(max-width: 595px)");
+
+  const [collapse, setCollapse] = useState(true);
+
+  const handleCollapse = () => {
+    setCollapse(!collapse);
+  };
+
   return (
-    <>
-      <StyledDivider />
-      <StyledBoardSection sectionBackground={sectionBackground}>
-        <Stack justifyContent="space-between" gap={inube.spacing.s200}>
-          <Text type="title" ellipsis>
+    <StyledBoardSection
+      $sectionBackground={sectionBackground}
+      $orientation={orientation}
+    >
+      <Stack
+        justifyContent={
+          orientation === "vertical" ? "space-between" : "flex-start"
+        }
+        alignItems="center"
+        gap={inube.spacing.s300}
+      >
+        <Stack alignItems="center" gap={inube.spacing.s100}>
+          {orientation !== "vertical" && (
+            <StyledCollapseIcon $collapse={collapse} onClick={handleCollapse}>
+              <Icon
+                icon={<MdOutlineChevronRight />}
+                appearance="dark"
+                size="26px"
+                cursorHover
+              />
+            </StyledCollapseIcon>
+          )}
+          <Text
+            type={
+              orientation === "vertical" || smallScreen ? "title" : "headline"
+            }
+            size={
+              orientation === "vertical" || smallScreen ? "large" : "medium"
+            }
+            ellipsis
+          >
             {sectionTitle}
           </Text>
-          <Text type="title" size="medium">
-            {numberActiveCards}
-          </Text>
         </Stack>
-        <Stack direction="column" gap={inube.spacing.s200}>
+        <Text type="title" size="medium">
+          {numberActiveCards}
+        </Text>
+      </Stack>
+      {collapse && (
+        <Stack
+          wrap="wrap"
+          direction={orientation === "vertical" ? "column" : "row"}
+          justifyContent={smallScreen ? "center" : "flex-start"}
+          gap={inube.spacing.s250}
+        >
           {children}
         </Stack>
-      </StyledBoardSection>
-    </>
+      )}
+    </StyledBoardSection>
   );
 }
 
--- src/components/layout/BoardSection/styles.ts
+++ src/components/layout/BoardSection/styles.ts
@@ -1,34 +1,43 @@
 import styled from "styled-components";
 import { inube } from "@inube/design-system";
 
-import { SectionBackground } from "./types";
+import { SectionBackground, SectionOrientation } from "./types";
 
 interface IStyledBoardSection {
   theme?: typeof inube;
-  sectionBackground?: SectionBackground;
+  $sectionBackground?: SectionBackground;
+  $orientation?: SectionOrientation;
+}
+
+interface IStyledCollapseIcon {
+  $collapse: boolean;
 }
 
 const StyledBoardSection = styled.div<IStyledBoardSection>`
   display: flex;
   flex-direction: column;
   gap: ${inube.spacing.s150};
-  padding: ${inube.spacing.s400} ${inube.spacing.s150};
-  width: 261px;
-  height: 1500px;
-  background-color: ${({ theme, sectionBackground }) =>
-    sectionBackground === "gray"
+  padding: ${inube.spacing.s300} ${inube.spacing.s150} ${inube.spacing.s150};
+  width: ${({ $orientation }) =>
+    $orientation === "horizontal" ? "calc(100% - 24px)" : "261px"};
+  min-width: 261px;
+  height: ${({ $orientation }) =>
+    $orientation === "horizontal" ? "100%" : "1500px"};
+  border-top: 1px solid;
+  border-top-color: ${({ theme }: IStyledBoardSection) =>
+    theme?.color?.stroke?.divider?.dark || inube.color.stroke.divider.dark};
+  background-color: ${({ theme, $sectionBackground }) =>
+    $sectionBackground === "gray"
       ? theme?.color?.surface?.gray?.regular || inube.color.surface.gray.regular
       : theme?.color?.surface?.light?.regular ||
         inube.color.surface.light.regular};
 `;
 
-const StyledDivider = styled.hr`
-  margin: ${inube.spacing.s0};
-  width: 285px;
-  border: none;
-  border-top: 1px solid;
-  border-top-color: ${({ theme }: IStyledBoardSection) =>
-    theme?.color?.stroke?.divider?.dark || inube.color.stroke.divider.dark};
+const StyledCollapseIcon = styled.div<IStyledCollapseIcon>`
+  display: flex;
+  transition: all 500ms ease;
+  transform: ${({ $collapse }) =>
+    $collapse ? "rotate(-90deg)" : "rotate(90deg)"};
 `;
 
-export { StyledBoardSection, StyledDivider };
+export { StyledBoardSection, StyledCollapseIcon };
