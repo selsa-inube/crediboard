205c0052fa76346bd37b60adb6d1d37accba6202
Andres Babativa <carlos3k11@gmail.com>
2024-04-15 16:23:28 -0500

    refactor(TableBoar): separate functions other than components into a file

---
+++ src/components/data/TableBoard/stories/utils.ts
@@ -0,0 +1,24 @@
+import { AppearenceTagObject, IEntries, IEntriesTranform } from "../types";
+
+export const appearenceTag = (
+  requirementTag: keyof typeof AppearenceTagObject
+) => AppearenceTagObject[requirementTag];
+
+export const transformData = (data: IEntries[]) => {
+  const transformed: IEntriesTranform[] = [];
+
+  data.forEach((item) => {
+    const sectionTitle = item.section.title;
+    const requirements = item.section.requirements;
+
+    requirements.forEach((requirement, index) => {
+      if (transformed.length <= index) {
+        transformed.push({ id: requirement.id });
+      }
+      transformed[index][sectionTitle] = requirement.description;
+      transformed[index][requirement.description] = requirement.tag;
+    });
+  });
+
+  return transformed;
+};
