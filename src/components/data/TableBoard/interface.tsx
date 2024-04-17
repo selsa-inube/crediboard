7192b574d04c3c5bc02584943abed5ed1161af77
Andres Babativa <carlos3k11@gmail.com>
2024-04-16 20:10:53 -0500

    refactor(TableBoard): adjust rendering of table parts according to props

--- src/components/data/TableBoard/interface.tsx
+++ src/components/data/TableBoard/interface.tsx
@@ -1,171 +1,63 @@
-import React from "react";
-import { Text, Tag, Icon } from "@inube/design-system";
-
-import { appearenceTag } from "./utils";
-import { AppearenceTagObject, IEntriesTranform } from "./types";
+import { Text } from "@inube/design-system";
 
 import {
   StyledContainer,
   StyledTable,
-  StyledTdTitle,
-  StyledTdbodyContainer,
   StyledTbody,
-  StyledTd,
-  StyledThTitle,
   StyledThead,
+  StyledTr,
+  StyledTh,
 } from "./styles";
 
 import { ITableBoardProps } from ".";
-import { MdAddCircleOutline } from "react-icons/md";
 
-interface ITableBordUIprops extends ITableBoardProps {
-  normalizedEntries: IEntriesTranform[];
+interface ITableBoardUIProps extends ITableBoardProps {
+  titlesList: string[];
 }
 
-const TitleTableBoard = (
-  props: Pick<ITableBoardProps, "entries" | "colspan">
-) => {
-  const { entries, colspan } = props;
-
-  const titles = entries.map((data) => ({
-    id: data.section.title,
-    titleName: data.section.title,
-    priority: data.section.priority,
-  }));
-
+export const TableBoardUI = (props: ITableBoardUIProps) => {
+  const { id, entries, actions, titles, titlesList, borderTable } = props;
   return (
-    <>
-      {titles &&
-        titles.map((title) => (
-          <StyledThTitle key={title.id} colSpan={colspan!}>
-            <Text appearance="primary" type="title" size="medium">
-              {title.titleName}
-            </Text>
-          </StyledThTitle>
-        ))}
-    </>
-  );
-};
-
-const EntriesSectionsTableBoard = (
-  props: Pick<ITableBoardProps, "entries">
-) => {
-  const { entries } = props;
-
-  return (
-    <>
-      {entries.map((entry) => (
-        <React.Fragment key={entry.section.title}>
+    <StyledContainer id={id} $borderTable={borderTable!}>
+      <StyledTable>
+        <StyledThead>
           <tr>
-            <StyledTdTitle>
-              <Text appearance="primary" type="title" size="medium">
-                {entry.section.title}
-              </Text>
-            </StyledTdTitle>
+            {titles.map((title) => (
+              <StyledTh key={title.id + id}>
+                <Text
+                  appearance="primary"
+                  type="title"
+                  size="medium"
+                  padding="4px"
+                >
+                  {title.titleName}
+                </Text>
+              </StyledTh>
+            ))}
           </tr>
-          {entry.section.requirements.map((requirement, index) => (
-            <StyledTdbodyContainer
-              key={requirement.id}
+        </StyledThead>
+        <StyledTbody>
+          {entries.map((entry, index) => (
+            <StyledTr
+              key={`${entry.id}-${index}`}
               $zebraEffect={index % 2 === 0}
             >
-              <td>
-                <Text size="small">{requirement.description}</Text>
-              </td>
-              {requirement.tag && (
-                <StyledTd>
-                  <Tag
-                    label={requirement.tag}
-                    appearance={appearenceTag(requirement.tag)}
-                  />
-                </StyledTd>
-              )}
-            </StyledTdbodyContainer>
+              {titlesList.map((title) => (
+                <td key={title}>
+                  {typeof entry[title] !== "string" ? (
+                    entry[title]
+                  ) : (
+                    <Text size="small" padding="4px">
+                      {entry[title]}
+                    </Text>
+                  )}
+                </td>
+              ))}
+              {actions?.map((action) => (
+                <td key={action.id}>{action.content(entry)}</td>
+              ))}
+            </StyledTr>
           ))}
-        </React.Fragment>
-      ))}
-    </>
-  );
-};
-
-const EntriesTableBoard = (
-  props: Pick<ITableBordUIprops, "entries" | "normalizedEntries">
-) => {
-  const { entries, normalizedEntries } = props;
-
-  return (
-    <>
-      {normalizedEntries.map((entry, index) => (
-        <StyledTdbodyContainer key={entry.id} $zebraEffect={index % 2 === 0}>
-          {entries.map((data) => (
-            <React.Fragment key={data.section.title}>
-              <td>
-                <Text size="small">{entry[data.section.title]}</Text>
-              </td>
-              {entry[entry[data.section.title]] && (
-                <StyledTd>
-                  <Tag
-                    label={entry[entry[data.section.title]]}
-                    appearance={appearenceTag(
-                      entry[
-                        entry[data.section.title]
-                      ] as keyof typeof AppearenceTagObject
-                    )}
-                  />
-                </StyledTd>
-              )}
-            </React.Fragment>
-          ))}
-        </StyledTdbodyContainer>
-      ))}
-    </>
-  );
-};
-
-/* const ActionsTableBoard = (props: Pick<ITableBoardProps, "actions">) => {
-  const { actions } = props;
-
-  return (
-    <>
-      {actions &&
-        actions.map((action) => (
-          <StyledTd key={action.id}>{action.content}</StyledTd>
-        ))}
-    </>
-  );
-};
- */
-export const TableBoardUI = (props: ITableBordUIprops) => {
-  const {
-    id,
-    entries,
-    withTitles,
-    colspan = "1",
-    normalizedEntries,
-    //actions,
-  } = props;
-
-  return (
-    <StyledContainer id={id}>
-      <StyledTable>
-        {!withTitles && (
-          <StyledThead>
-            <tr>
-              <TitleTableBoard entries={entries} colspan={colspan} />
-            </tr>
-          </StyledThead>
-        )}
-        <StyledTbody>
-          {withTitles ? (
-            <EntriesSectionsTableBoard entries={entries} />
-          ) : (
-            <EntriesTableBoard
-              entries={entries}
-              normalizedEntries={normalizedEntries}
-            />
-          )}
-          <tr>
-            <Icon icon={<MdAddCircleOutline />} appearance="primary" />
-          </tr>
         </StyledTbody>
       </StyledTable>
     </StyledContainer>
