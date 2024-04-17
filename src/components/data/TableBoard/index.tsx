18d0a79482c23ba82baa909932a348074564eb43
Andres Babativa <carlos3k11@gmail.com>
2024-04-16 19:59:34 -0500

    feat(TableBoard): include actions and update component interface

--- src/components/data/TableBoard/index.tsx
+++ src/components/data/TableBoard/index.tsx
@@ -1,30 +1,26 @@
-import { transformData } from "./utils";
 import { TableBoardUI } from "./interface";
-import { IAction, IEntries } from "./types";
+import { IAction, IEntries, ITitle } from "./types";
 
 export interface ITableBoardProps {
   id: string;
-  withTitles: boolean;
-  colspan?: string;
+  borderTable?: boolean;
+  titles: ITitle[];
   entries: IEntries[];
   actions?: IAction[];
 }
 
 export const TableBoard = (props: ITableBoardProps) => {
-  const { id, entries, withTitles, actions, colspan = "1" } = props;
-
-  const normalizedEntries = transformData(entries);
-
-  console.log(actions?.length, "actions");
+  const { id, entries, actions, titles, borderTable = false } = props;
+  const titlesList = Object.keys(entries[0]).filter((key) => key !== "id");
 
   return (
     <TableBoardUI
       id={id}
       entries={entries}
-      withTitles={withTitles}
-      colspan={colspan}
-      normalizedEntries={normalizedEntries}
+      titles={titles}
       actions={actions}
+      borderTable={borderTable}
+      titlesList={titlesList}
     />
   );
 };
