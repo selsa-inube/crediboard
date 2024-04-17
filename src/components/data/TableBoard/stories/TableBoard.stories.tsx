222159cbe8100b4f53067528105f865e4e49cdac
Andres Babativa <carlos3k11@gmail.com>
2024-04-16 21:34:58 -0500

    refactor(TableBoard.stories): include props in argTypes

--- src/components/data/TableBoard/stories/TableBoard.stories.tsx
+++ src/components/data/TableBoard/stories/TableBoard.stories.tsx
@@ -2,12 +2,14 @@ import type { Meta, StoryObj } from "@storybook/react";
 
 import { actionsMock, mockData, titlesMock } from "./mockStories";
 import { TableBoard } from "..";
+import { props } from "./props";
 
 type Story = StoryObj<typeof TableBoard>;
 
 const meta: Meta<typeof TableBoard> = {
   title: "components/data/TableBoard",
   component: TableBoard,
+  argTypes: props,
 };
 
 export const Default: Story = {
