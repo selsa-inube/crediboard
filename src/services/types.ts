d5365089bb90e3d80f14f1be9146df89b4abd9d6
carin <ca.rincon97@gmail.com>
2024-04-18 04:17:32 -0500

    fix: adjusting documentation and types db

--- src/services/types.ts
+++ src/services/types.ts
@@ -5,7 +5,7 @@ interface Requests {
   v_Monto: number;
   k_Idterc: number;
   k_Desdin: string;
-  i_Estprs: string;
+  i_Estprs: DmEtapasPrs;
   n_Desdin: string;
   aanumnit: string;
   nnasocia: string;
@@ -13,4 +13,49 @@ interface Requests {
   n_Descr_Tarea: string;
 }
 
-export type { Requests };
+type DmEtapasPrs =
+  | "CUMPLIMIENTO_REQUISITOS"
+  | "FORMALIZACION_GARANTIAS"
+  | "GESTION_COMERCIAL"
+  | "TRAMITADA"
+  | "TRAMITE_DESEMBOLSO"
+  | "VERIFICACION_APROBACION";
+
+type DmTareasPrs =
+  | "ASESORAR_CLIENTE"
+  | "CONFIRMAR_APROBACION"
+  | "CONFIRMAR_DESEMBOLSO"
+  | "GARANTIZAR_SOPORTES_POS"
+  | "VALIDAR_REQUISITOS"
+  | "VALIDAR_SOPORTES_JURIDICOS"
+  | "INTERFACE_CARTERA";
+
+type DmConceptos =
+  | "APROBAR_SOLICITUD"
+  | "AUTOMATICA"
+  | "GESTION_COMERCIAL"
+  | "ANALISIS_RIESGO"
+  | "RECHAZAR_SOLICITUD"
+  | "SIN_CONCEPTO"
+  | "APROBACION_HUMANA"
+  | "RECHAZO_HUMANO";
+
+type DmDecisions =
+  | "ANALISIS_RIESGO"
+  | "ANULAR_SOLICITUD"
+  | "APROBAR_SOLICITUD"
+  | "CANCELAR_SOLICITUD"
+  | "CONFIRMACION_CLIENTE"
+  | "DESEMBOLSO_DECLINADO"
+  | "DESEMBOLSO_REALIZADO"
+  | "GESTION_COMERCIAL"
+  | "RECHAZAR_SOLICITUD"
+  | "SOPORTES_INVALIDOS"
+  | "SOPORTES_POS_NO_RECIBIDOS"
+  | "SOPORTES_POS_RECIBIDOS"
+  | "SOPORTES_VALIDOS"
+  | "VIABILIZAR_SOLICITUD"
+  | "CREAR_OBLIGACIONES_DE_CARTERA"
+  | "DECLINAR_OBLIGACIONES_DE_CARTERA";
+
+export type { Requests, DmEtapasPrs, DmTareasPrs, DmConceptos, DmDecisions };
