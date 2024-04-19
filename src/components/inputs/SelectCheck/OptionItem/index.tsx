c0d1ba4b64891ffbf523ea4b320443b9edc91c5c
Andres Babativa <carlos3k11@gmail.com>
2024-04-19 16:14:58 -0500

    feat(SelectCheck.OptionItem): to include checked and onchange props

--- src/components/inputs/SelectCheck/OptionItem/index.tsx
+++ src/components/inputs/SelectCheck/OptionItem/index.tsx
@@ -1,18 +1,28 @@
 import { Label, Stack } from "@inube/design-system";
+
 import { StyledOptionItem, StyledInput } from "./styles";
 
 export interface IOptionItemProps {
   id: string;
   label: string;
+  checked?: boolean;
+  onchange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
 export const OptionItem = (props: IOptionItemProps) => {
-  const { id, label } = props;
+  const { id, label, checked, onchange } = props;
 
   return (
-    <StyledOptionItem id={id}>
+    <StyledOptionItem>
       <Stack>
-        <StyledInput type="checkbox" name={id} />
+        <StyledInput
+          readOnly
+          type="checkbox"
+          id={id}
+          name={id}
+          onChange={onchange}
+          checked={checked}
+        />
         <Label htmlFor={id}>{label}</Label>
       </Stack>
     </StyledOptionItem>
