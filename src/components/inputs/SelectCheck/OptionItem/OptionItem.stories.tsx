37c2addb7a4654ca6e0cc9baf5cb567d2022dcbb
Andres Babativa <carlos3k11@gmail.com>
2024-04-22 17:54:52 -0500

    docs(OptionItem): implement OptionList story

---
+++ src/components/inputs/SelectCheck/OptionItem/OptionItem.stories.tsx
@@ -0,0 +1,22 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import { parameters, props } from "./props";
+import { OptionItem } from ".";
+
+type Story = StoryObj<typeof OptionItem>;
+
+const meta: Meta<typeof OptionItem> = {
+  title: "components/inputs/SelectCheck/OptionItem",
+  component: OptionItem,
+  parameters,
+  argTypes: props,
+};
+
+export const Default: Story = {
+  args: {
+    id: "1",
+    label: "Item 1",
+  },
+};
+
+export default meta;
