c9d37fc7dcf6bc5eb613a4c7b62202f800f776ec
Andres Babativa <carlos3k11@gmail.com>
2024-04-23 16:42:45 -0500

    refactor(FinancialReporting): remove unused ContainerSections import in board.tsx

--- src/routes/board.tsx
+++ src/routes/board.tsx
@@ -1,7 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 
 import { ErrorPage } from "@src/components/layout/ErrorPage";
-import { ContainerSections } from "@src/components/layout/ContainerSections";
+//import { ContainerSections } from "@src/components/layout/ContainerSections";
 import { Board } from "@src/pages/board";
 import { BoardLayout } from "@pages/board/outlets/boardlayout";
 
@@ -10,10 +10,6 @@ function BoardRoutes() {
     <Routes>
       <Route path="/" element={<Board />}>
         <Route path="/" element={<BoardLayout />} />
-        <Route
-          path="/solicitud/:solicitud_id"
-          element={<ContainerSections />}
-        />
       </Route>
       <Route path="/*" element={<ErrorPage />} />
     </Routes>
