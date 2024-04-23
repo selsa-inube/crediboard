2f647249f4bce70c268dfbf8f5db4e658daf7c2f
Andres Babativa <carlos3k11@gmail.com>
2024-04-23 11:31:56 -0500

    refactor(FinancialReporting): use individual props for each section

--- src/pages/board/outlets/financialReporting/index.tsx
+++ src/pages/board/outlets/financialReporting/index.tsx
@@ -3,33 +3,42 @@ import { Stack, inube, Text, Grid } from "@inube/design-system";
 import { ContainerSections } from "@components/layout/ContainerSections";
 import { Fieldset } from "@components/data/Fieldset";
 
-export interface IFinancialReportingSectionsProps {
-  children?: JSX.Element | JSX.Element[];
+export interface IFinancialReportingProps {
+  toDo?: JSX.Element | JSX.Element[];
+  requirements?: JSX.Element | JSX.Element[];
+  promissoryNotes?: JSX.Element | JSX.Element[];
+  approvals?: JSX.Element | JSX.Element[];
+  management?: JSX.Element | JSX.Element[];
+  postingVouchers?: JSX.Element | JSX.Element[];
 }
 
-const FinancialReportingSections = (
-  props: IFinancialReportingSectionsProps
-) => {
-  const { children } = props;
-  return (
-    <Stack direction="column" gap={inube.spacing.s500}>
-      <Stack direction="column">
-        <Fieldset title="Estado" descriptionTitle="Gestión Comercial">
-          <Text></Text>
-        </Fieldset>
-      </Stack>
-      <Grid templateColumns="repeat(2,1fr)" gap="s200" autoRows="auto">
-        {children}
-      </Grid>
-    </Stack>
-  );
-};
-
-export const FinancialReporting = () => {
+export const FinancialReporting = (props: IFinancialReportingProps) => {
+  const {
+    toDo,
+    requirements,
+    promissoryNotes,
+    approvals,
+    management,
+    postingVouchers,
+  } = props;
   return (
     <Stack direction="column" margin="s250">
       <ContainerSections>
-        <FinancialReportingSections />
+        <Stack direction="column" gap={inube.spacing.s500}>
+          <Stack direction="column">
+            <Fieldset title="Estado" descriptionTitle="Gestión Comercial">
+              <Text></Text>
+            </Fieldset>
+          </Stack>
+          <Grid templateColumns="repeat(2,1fr)" gap="s200" autoRows="auto">
+            <Stack direction="column">{toDo}</Stack>
+            <Stack direction="column">{approvals}</Stack>
+            <Stack direction="column">{requirements}</Stack>
+            <Stack direction="column">{management}</Stack>
+            <Stack direction="column">{promissoryNotes}</Stack>
+            <Stack direction="column">{postingVouchers}</Stack>
+          </Grid>
+        </Stack>
       </ContainerSections>
     </Stack>
   );
