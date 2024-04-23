017463d465d67f40953aa69267cee6feb03c587c
Andres Babativa <carlos3k11@gmail.com>
2024-04-23 12:03:44 -0500

    refactor(FinancialReporting): implement props children

--- src/pages/board/outlets/financialReporting/index.tsx
+++ src/pages/board/outlets/financialReporting/index.tsx
@@ -1,26 +1,15 @@
-import { Stack, inube, Text, Grid } from "@inube/design-system";
+import { Stack, inube, Text, Grid, useMediaQuery } from "@inube/design-system";
 
 import { ContainerSections } from "@components/layout/ContainerSections";
 import { Fieldset } from "@components/data/Fieldset";
 
 export interface IFinancialReportingProps {
-  toDo?: JSX.Element | JSX.Element[];
-  requirements?: JSX.Element | JSX.Element[];
-  promissoryNotes?: JSX.Element | JSX.Element[];
-  approvals?: JSX.Element | JSX.Element[];
-  management?: JSX.Element | JSX.Element[];
-  postingVouchers?: JSX.Element | JSX.Element[];
+  children?: JSX.Element | JSX.Element[];
 }
 
 export const FinancialReporting = (props: IFinancialReportingProps) => {
-  const {
-    toDo,
-    requirements,
-    promissoryNotes,
-    approvals,
-    management,
-    postingVouchers,
-  } = props;
+  const { children } = props;
+  const isMobile = useMediaQuery("(max-width: 768px)");
   return (
     <Stack direction="column" margin="s250">
       <ContainerSections>
@@ -30,13 +19,12 @@ export const FinancialReporting = (props: IFinancialReportingProps) => {
               <Text></Text>
             </Fieldset>
           </Stack>
-          <Grid templateColumns="repeat(2,1fr)" gap="s200" autoRows="auto">
-            <Stack direction="column">{toDo}</Stack>
-            <Stack direction="column">{approvals}</Stack>
-            <Stack direction="column">{requirements}</Stack>
-            <Stack direction="column">{management}</Stack>
-            <Stack direction="column">{promissoryNotes}</Stack>
-            <Stack direction="column">{postingVouchers}</Stack>
+          <Grid
+            templateColumns={isMobile ? "1fr" : "repeat(2,1fr)"}
+            gap="s200"
+            autoRows="auto"
+          >
+            {children}
           </Grid>
         </Stack>
       </ContainerSections>
