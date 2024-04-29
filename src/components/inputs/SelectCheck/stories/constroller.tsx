c8223694a662aaeda55e19761dac84c0b3bea5b2
Andres Babativa <carlos3k11@gmail.com>
2024-04-29 11:59:20 -0500

    refactor(Selectcheck): implement a way in which checking can be controlled

--- src/components/inputs/SelectCheck/stories/constroller.tsx
+++ src/components/inputs/SelectCheck/stories/constroller.tsx
@@ -7,6 +7,20 @@ export const ControllerSelectCheck = (props: ISelectProps) => {
   const [data, setData] = useState(options);
 
   const handleToggleEntry = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.tagName === "LI") {
+      const elementInput = e.target.querySelector("input");
+      if (elementInput) {
+        const { name, checked } = elementInput;
+        const newData = data.map((entry) => {
+          if (entry.id === name) {
+            return { ...entry, checked: !checked };
+          }
+          return entry;
+        });
+        return setData(newData);
+      }
+    }
+
     const { name, checked } = e.target;
     const newData = data.map((entry) => {
       if (entry.id === name) {
