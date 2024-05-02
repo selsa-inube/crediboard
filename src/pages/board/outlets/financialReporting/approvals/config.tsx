d73ed1de62dfc01314d9530fcc38c3f07ac263d6
Andres Babativa <carlos3k11@gmail.com>
2024-05-01 20:02:58 -0500

    feat(Approbals.config): add MdWarningAmber icon to error field

--- src/pages/board/outlets/financialReporting/approvals/config.tsx
+++ src/pages/board/outlets/financialReporting/approvals/config.tsx
@@ -1,5 +1,5 @@
 import { isValidElement } from "react";
-import { MdNotificationsNone } from "react-icons/md";
+import { MdNotificationsNone, MdWarningAmber } from "react-icons/md";
 import { Icon, Tag } from "@inube/design-system";
 
 import { IEntries } from "@components/data/TableBoard/types";
@@ -19,6 +19,11 @@ export const titlesApprobals = [
     titleName: "Decisión",
     priority: 2,
   },
+  {
+    id: "error",
+    titleName: "Error",
+    priority: 3,
+  },
 ];
 
 export const entriesApprobals = [
@@ -26,41 +31,65 @@ export const entriesApprobals = [
     id: "uno",
     usuarios: "Pedro Pablo Iregui Gerrero",
     decision: <Tag label="Aprobado" appearance="success" />,
+    error: "",
   },
   {
     id: "dos",
     usuarios: "Carlos Alberto Combita",
     decision: <Tag label="Rechazado" appearance="error" />,
+    error: (
+      <Icon
+        icon={<MdWarningAmber />}
+        appearance="warning"
+        spacing="compact"
+        cursorHover
+        size="24px"
+      />
+    ),
   },
   {
     id: "tres",
     usuarios: "Jaime Alberto Linares Guacaneme",
     decision: <Tag label="Aprovado" appearance="success" />,
+    erro: "",
   },
   {
     id: "cuatro",
     usuarios: "Miguel Angel Fuentes",
     decision: <Tag label="Pendiente" appearance="warning" />,
+    error: "",
   },
   {
     id: "cinco",
     usuarios: "Cesar Augusto Corredor",
     decision: <Tag label="Aprobado" appearance="success" />,
+    error: "",
   },
   {
     id: "seis",
     usuarios: "Paula Andrea Betancurt",
     decision: <Tag label="Rechazado" appearance="error" />,
+    error: (
+      <Icon
+        icon={<MdWarningAmber />}
+        appearance="warning"
+        spacing="compact"
+        cursorHover
+        size="24px"
+      />
+    ),
   },
   {
     id: "siete",
     usuarios: "Jaime Alejandro Vargas",
     decision: <Tag label="Pendiente" appearance="warning" />,
+    error: "",
   },
   {
     id: "ocho",
     usuarios: "Viviana Amador Tejada",
     decision: <Tag label="Aprobado" appearance="success" />,
+    error: "",
   },
 ];
 
