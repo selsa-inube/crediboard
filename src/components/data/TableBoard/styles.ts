72fc2e554f1da98847393f379b41d6fb71f1c6e3
Andres Babativa <carlos3k11@gmail.com>
2024-06-04 18:01:56 -0500

    refactor(TableBoard): fix overflow issue and add zebra effect to appearanceTable

--- src/components/data/TableBoard/styles.ts
+++ src/components/data/TableBoard/styles.ts
@@ -18,7 +18,7 @@ interface IStyledTd {
 
 export const StyledContainer = styled.div<IStyledContainer>`
   border-radius: 8px;
-  //overflow: hidden;
+  overflow: hidden;
   padding-top: ${({ theme }) => theme?.spacing?.s150 || inube.spacing.s150};
   padding-bottom: ${({ theme }) => theme?.spacing?.s150 || inube.spacing.s150};
   padding-left: ${({ theme }) => theme?.spacing?.s075 || inube.spacing.s075};
@@ -31,9 +31,18 @@ export const StyledContainer = styled.div<IStyledContainer>`
   position: relative;
 `;
 
-export const StyledTable = styled.table`
+interface IStyledTable {
+  $zebraEffect?: boolean;
+}
+
+export const StyledTable = styled.table<IStyledTable>`
   border-collapse: collapse;
   width: 100%;
+  box-sizing: border-box;
+
+  ${({ $zebraEffect, theme }) =>
+    $zebraEffect &&
+    `tbody tr:nth-child(odd) {background-color: ${theme?.color?.surface?.gray?.clear || inube.color.surface.gray.clear};}; tbody tr:nth-child(even) {background-color: ${theme?.color?.surface?.gray?.regular || inube.color.surface.gray.regular};}`}
 `;
 
 export const StyledTbody = styled.tbody`
@@ -51,30 +60,34 @@ export const StyledTh = styled.th`
     theme?.color?.surface?.gray?.clear || inube.color.surface.gray.clear};
 `;
 
-export const StyledThactions = styled.th`
+interface IStyledThactions {
+  $right: number;
+  $isTablet: boolean;
+}
+export const StyledThactions = styled.th<IStyledThactions>`
   text-align: center;
-  min-width: 100px;
-  position: sticky;
-  right: 0;
-  z-index: 2;
+  // min-width: 100px;
+  //width: 100px;
+  ${({ $isTablet, $right }) =>
+    $isTablet && `position: sticky; right: ${$right}px; z-index: 22;`};
 `;
 
-export const StyledTdactions = styled.td`
-  text-align: -webkit-center;
-  min-width: 100%;
-  position: sticky;
-  right: 0;
+interface IStyledTdactions {
+  $right: number;
+  $isTablet: boolean;
+}
 
-  z-index: 1;
+export const StyledTdactions = styled.td<IStyledTdactions>`
+  text-align: -webkit-center;
+  ${({ $isTablet, $right }) =>
+    $isTablet && `position: sticky; right: ${$right}px; z-index: 22;`};
 `;
 
 export const StyledTr = styled.tr<IStyledTdbodyContainer>`
   vertical-align: middle;
   white-space: nowrap;
-  background-color: ${({ theme, $zebraEffect }) =>
-    $zebraEffect
-      ? theme?.color?.surface?.gray?.regular || inube.color.surface.gray.regular
-      : theme?.color?.surface?.gray?.clear || inube.color.surface.gray.clear};
+  box-sizing: border-box;
+
   border-bottom: ${({ theme, $borderTable }) =>
     $borderTable &&
     `1px solid ${theme?.color?.stroke?.divider?.regular || inube.color.stroke.divider.regular}`};
