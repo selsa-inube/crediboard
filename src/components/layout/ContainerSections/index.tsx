f9c38a35288b424806064e3d683278c8bc988e74
Andres Babativa <carlos3k11@gmail.com>
2024-06-14 19:08:13 -0500

    feat: add responsive behavior to ContainerSections component

--- src/components/layout/ContainerSections/index.tsx
+++ src/components/layout/ContainerSections/index.tsx
@@ -1,6 +1,13 @@
 import { useNavigate } from "react-router-dom";
-import { MdArrowBack } from "react-icons/md";
-import { Button, Stack, inube } from "@inube/design-system";
+import { MdArrowBack, MdMenu } from "react-icons/md";
+import {
+  Button,
+  Icon,
+  Stack,
+  Text,
+  inube,
+  useMediaQuery,
+} from "@inube/design-system";
 import { useState } from "react";
 
 import { TextAreaModal } from "@components/modals/TextAreaModal";
@@ -16,6 +23,8 @@ export const ContainerSections = (props: IContainerSectionsProps) => {
   const { children } = props;
   const [showRejectionModal, setShowRejectionModal] = useState(false);
 
+  const isMobile: boolean = useMediaQuery("(max-width: 720px)");
+
   const handleToggleRejectModal = () => {
     setShowRejectionModal(!showRejectionModal);
   };
@@ -30,34 +39,56 @@ export const ContainerSections = (props: IContainerSectionsProps) => {
     <>
       <Stack width="-webkit-fill-available" direction="column">
         <Stack direction="column">
-          <Stack justifyContent="start" margin="s0 s0 s250">
-            <Button
-              spacing="compact"
-              variant="none"
-              iconBefore={<MdArrowBack />}
-              onClick={() => navigation(-1)}
-            >
-              Volver
-            </Button>
+          <Stack justifyContent="space-between" margin="s0 s0 s250">
+            {!isMobile ? (
+              <Button
+                spacing="compact"
+                variant="none"
+                iconBefore={<MdArrowBack />}
+                onClick={() => navigation(-1)}
+              >
+                Volver
+              </Button>
+            ) : (
+              <Stack alignItems="center">
+                <Icon
+                  icon={<MdArrowBack />}
+                  appearance="primary"
+                  size="32px"
+                  spacing="none"
+                />
+                <Text>Volver</Text>
+              </Stack>
+            )}
+            {isMobile && (
+              <Icon
+                icon={<MdMenu />}
+                appearance="dark"
+                size="32px"
+                spacing="none"
+              />
+            )}
           </Stack>
-          <Stack justifyContent="end" gap={inube.spacing.s200}>
-            <Stack gap={inube.spacing.s400}>
-              <Button onClick={handleToggleRejectModal}>
-                {configButtons.buttons.buttonOne.label}
-              </Button>
-              <Button>{configButtons.buttons.buttonTwo.label}</Button>
-              <Button>{configButtons.buttons.buttonTree.label}</Button>
+          {!isMobile && (
+            <Stack justifyContent="end" gap={inube.spacing.s200}>
+              <Stack gap={inube.spacing.s400}>
+                <Button onClick={handleToggleRejectModal}>
+                  {configButtons.buttons.buttonOne.label}
+                </Button>
+                <Button>{configButtons.buttons.buttonTwo.label}</Button>
+                <Button>{configButtons.buttons.buttonTree.label}</Button>
+              </Stack>
+              <StyledHorizontalDivider />
+              <Stack gap={inube.spacing.s200}>
+                <Button variant="outlined">
+                  {configButtons.buttonsOutlined.buttonOne.label}
+                </Button>
+                <Button variant="outlined">
+                  {configButtons.buttonsOutlined.buttonTwo.label}
+                </Button>
+              </Stack>
             </Stack>
-            <StyledHorizontalDivider />
-            <Stack gap={inube.spacing.s200}>
-              <Button variant="outlined">
-                {configButtons.buttonsOutlined.buttonOne.label}
-              </Button>
-              <Button variant="outlined">
-                {configButtons.buttonsOutlined.buttonTwo.label}
-              </Button>
-            </Stack>
-          </Stack>
+          )}
         </Stack>
         <Stack direction="column">{children}</Stack>
       </Stack>
