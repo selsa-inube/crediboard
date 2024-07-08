4748603c03258f260eccc35c7a9821a66534c9a2
Angelbt12 <angeldavidbaracaldo056@gmail.com>
2024-07-08 14:05:04 -0500

    feat: notification modal

--- src/pages/board/outlets/financialReporting/Approvals/index.tsx
+++ src/pages/board/outlets/financialReporting/Approvals/index.tsx
@@ -1,13 +1,15 @@
+import { useState, useEffect } from "react";
 import { Fieldset } from "@components/data/Fieldset";
 import { TableBoard } from "@src/components/data/TableBoard";
 import { actionsApprovals, handleData, titlesApprovals } from "./config";
-import { useEffect, useState } from "react";
 import { IEntries } from "@src/components/data/TableBoard/types";
+import { NotificationModal } from "@components/modals/NotificationModal";
 
 export const Approvals = () => {
   const [entriesApprovals, setEntriesApprovals] = useState<IEntries[]>([]);
-
   const [loading, setLoading] = useState(false);
+  const [showModal, setShowModal] = useState(false);
+  const [selectedData, setSelectedData] = useState<IEntries | null>(null);
 
   useEffect(() => {
     setLoading(true);
@@ -17,15 +19,47 @@ export const Approvals = () => {
     });
   }, []);
 
+  const handleNotificationClick = (data: IEntries) => {
+    setSelectedData(data);
+    setShowModal(true);
+  };
+
+  const handleCloseModal = () => {
+    setShowModal(false);
+  };
+
+  const handleSubmit = (values: { textarea: string }) => {
+    console.log("Valores enviados:", values);
+    handleCloseModal();
+  };
+
   return (
-    <Fieldset title="Aprobaciones" heigthFieldset="282px" aspectRatio="3/1">
-      <TableBoard
-        id="usuarios"
-        titles={titlesApprovals}
-        entries={entriesApprovals}
-        actions={actionsApprovals}
-        loading={loading}
-      />
-    </Fieldset>
+    <>
+      <Fieldset title="Aprobaciones" heigthFieldset="282px" aspectRatio="3/1">
+        <TableBoard
+          id="usuarios"
+          titles={titlesApprovals}
+          entries={entriesApprovals}
+          actions={actionsApprovals.map((action) => ({
+            ...action,
+            content: (data: IEntries) => (
+              <div onClick={() => handleNotificationClick(data)}>
+                {action.content(data)}
+              </div>
+            ),
+          }))}
+          loading={loading}
+        />
+      </Fieldset>
+      {showModal && selectedData && (
+        <NotificationModal
+          title="Notificación"
+          buttonText="Enviar"
+          confirmationText="Esta seguro que deseas enviar esta solicitud para aprobación... la necesidad de evaluar esta solicitud."
+          onSubmit={handleSubmit}
+          onCloseModal={handleCloseModal}
+        />
+      )}
+    </>
   );
 };
