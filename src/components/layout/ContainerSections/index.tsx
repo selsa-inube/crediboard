bf356af02545a8931f8ec41ce77a0b8bad5a346c
Andres Babativa <carlos3k11@gmail.com>
2024-07-09 15:45:53 -0500

    refactor(ContainerSections): update attachment modal state variable names

--- src/components/layout/ContainerSections/index.tsx
+++ src/components/layout/ContainerSections/index.tsx
@@ -1,61 +1,29 @@
-import { useNavigate } from "react-router-dom";
-import { MdArrowBack, MdOutlineRemoveRedEye } from "react-icons/md";
-import { Button, Icon, Stack, Text, inube } from "@inube/design-system";
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { MdArrowBack, MdMenu } from "react-icons/md";
+import {
+  Button,
+  Icon,
+  Stack,
+  Text,
+  inube,
+  useMediaQuery,
+} from "@inube/design-system";
 
 import { TextAreaModal } from "@components/modals/TextAreaModal";
-
-import { configButtons, configDataAttachments } from "./config";
-import { StyledHorizontalDivider, StyledItem } from "./styles";
-import { Listmodal } from "@components/modals/Listmodal";
+import { configButtons } from "./config";
+import { StyledHorizontalDivider } from "./styles";
 
 interface IContainerSectionsProps {
   children?: JSX.Element | JSX.Element[];
 }
 
-interface IListdataProps {
-  data: { id: string; name: string }[];
-}
-
-const Listdata = (props: IListdataProps) => {
-  const { data } = props;
-
-  return (
-    <ul
-      style={{
-        paddingInlineStart: "2px",
-        marginBlock: "8px",
-      }}
-    >
-      {data.map((element) => (
-        <StyledItem key={element.id}>
-          <Text>{element.name}</Text>
-          <Icon
-            icon={<MdOutlineRemoveRedEye />}
-            appearance="dark"
-            spacing="none"
-            size="24px"
-            cursorHover
-          />
-        </StyledItem>
-      ))}
-    </ul>
-  );
-};
-
 export const ContainerSections = (props: IContainerSectionsProps) => {
   const { children } = props;
+
   const [showRejectionModal, setShowRejectionModal] = useState(false);
 
-  const [attachDocuments, setAttachDocuments] = useState(false);
-
-  const handleToggleRejectModal = () => {
-    setShowRejectionModal(!showRejectionModal);
-  };
-
-  const handleSubmitRejectModal = () => {
-    setShowRejectionModal(!showRejectionModal);
-  };
+  const isMobile: boolean = useMediaQuery("(max-width: 720px)");
 
   const navigation = useNavigate();
 
@@ -63,44 +31,63 @@ export const ContainerSections = (props: IContainerSectionsProps) => {
     <>
       <Stack width="-webkit-fill-available" direction="column">
         <Stack direction="column">
-          <Stack justifyContent="start" margin="s0 s0 s250">
-            <Button
-              spacing="compact"
-              variant="none"
-              iconBefore={<MdArrowBack />}
-              onClick={() => navigation(-1)}
-            >
-              Volver
-            </Button>
-          </Stack>
-          <Stack justifyContent="end" gap={inube.spacing.s200}>
-            <Stack gap={inube.spacing.s400}>
-              <Button onClick={handleToggleRejectModal}>
-                {configButtons.buttons.buttonOne.label}
-              </Button>
-              <Button>{configButtons.buttons.buttonTwo.label}</Button>
-              <Button>{configButtons.buttons.buttonTree.label}</Button>
-            </Stack>
-            <StyledHorizontalDivider />
-            <Stack gap={inube.spacing.s200}>
-              <Button variant="outlined">
-                {configButtons.buttonsOutlined.buttonOne.label}
-              </Button>
+          <Stack justifyContent="space-between" margin="s0 s0 s250">
+            {!isMobile ? (
               <Button
-                variant="outlined"
-                onClick={() => setAttachDocuments(true)}
+                spacing="compact"
+                variant="none"
+                iconBefore={<MdArrowBack />}
+                onClick={() => navigation(-1)}
               >
-                {configButtons.buttonsOutlined.buttonTwo.label}
+                Volver
               </Button>
-              {attachDocuments && (
-                <Listmodal
-                  title="Ver Adjuntos"
-                  content={<Listdata data={configDataAttachments} />}
-                  handleClose={() => setAttachDocuments(false)}
+            ) : (
+              <Stack alignItems="center">
+                <Icon
+                  icon={<MdArrowBack />}
+                  appearance="primary"
+                  size="32px"
+                  spacing="none"
+                  onClick={() => navigation(-1)}
                 />
-              )}
-            </Stack>
+                <Text>Volver</Text>
+              </Stack>
+            )}
+            {isMobile && (
+              <Icon
+                icon={<MdMenu />}
+                appearance="dark"
+                size="32px"
+                spacing="none"
+              />
+            )}
           </Stack>
+          {!isMobile && (
+            <Stack
+              justifyContent="end"
+              gap={inube.spacing.s200}
+              margin={!isMobile ? "s0 s0 s200 s0" : "s0"}
+            >
+              <Stack gap={inube.spacing.s400}>
+                <Button
+                  onClick={() => setShowRejectionModal(!showRejectionModal)}
+                >
+                  {configButtons.buttons.buttonOne.label}
+                </Button>
+                <Button>{configButtons.buttons.buttonTwo.label}</Button>
+                <Button>{configButtons.buttons.buttonTree.label}</Button>
+              </Stack>
+              <StyledHorizontalDivider />
+              <Stack gap={inube.spacing.s200}>
+                <Button variant="outlined">
+                  {configButtons.buttonsOutlined.buttonOne.label}
+                </Button>
+                <Button variant="outlined">
+                  {configButtons.buttonsOutlined.buttonTwo.label}
+                </Button>
+              </Stack>
+            </Stack>
+          )}
         </Stack>
         <Stack direction="column">{children}</Stack>
       </Stack>
@@ -110,8 +97,8 @@ export const ContainerSections = (props: IContainerSectionsProps) => {
           buttonText="Confirmar"
           inputLabel="Motivo del rechazo."
           inputPlaceholder="Describa el motivo del rechazo."
-          onCloseModal={handleToggleRejectModal}
-          onSubmit={handleSubmitRejectModal}
+          onCloseModal={() => setShowRejectionModal(!showRejectionModal)}
+          onSubmit={() => setShowRejectionModal(!showRejectionModal)}
         />
       )}
     </>
