338511852a556a83b4227a3e2c392e8df140ea68
Andres Babativa <carlos3k11@gmail.com>
2024-07-11 08:43:13 -0500

    chore(utils): adjust margins and improve performance in generatePDF function

--- src/utils/pdf/generetePDF.ts
+++ src/utils/pdf/generetePDF.ts
@@ -4,9 +4,10 @@ import html2canvas from "html2canvas";
 
 export const generatePDF = async (
   content: JSX.Element,
-  titleSave = "document.pdf"
+  titleSave = "document"
 ) => {
   const containerPDF = document.createElement("div");
+  containerPDF.style.height = "100vh";
 
   document.body.appendChild(containerPDF);
 
@@ -15,24 +16,20 @@ export const generatePDF = async (
 
   await new Promise((resolve) => setTimeout(resolve, 1000));
 
-  const canvas = await html2canvas(containerPDF, {
-    height: containerPDF.scrollHeight,
-    width: containerPDF.scrollWidth,
-    useCORS: true,
-    allowTaint: true,
-  });
+  const canvas = await html2canvas(containerPDF);
 
   const imgData = canvas.toDataURL("image/png");
   const pdf = new jsPDF("l", "mm", "a4");
+
   const imgProps = pdf.getImageProperties(imgData);
   const pdfWidth = pdf.internal.pageSize.getWidth();
   const pdfHeight = (imgProps.height * pdfWidth) / imgProps.width;
 
   const margins = {
     top: 20,
-    bottom: 0,
-    left: 25.4,
-    right: 25.4,
+    bottom: 20,
+    left: 15.4,
+    right: 15.4,
   };
 
   const contentWidth = pdfWidth - margins.left - margins.right;
@@ -47,7 +44,7 @@ export const generatePDF = async (
     contentHeight
   );
 
-  pdf.save(`${titleSave}.pdf`);
+  pdf.save(titleSave);
 
   document.body.removeChild(containerPDF);
 };
