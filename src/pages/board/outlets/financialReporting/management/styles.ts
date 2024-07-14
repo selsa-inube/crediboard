c3000d9583a5a45cd765e4392478fa31a5e20c7b
Brandonz07 <brodriguez@sistemasenlinea.com.co>
2024-07-14 13:49:41 -0500

    fix: fixed chat height

--- src/pages/board/outlets/financialReporting/management/styles.ts
+++ src/pages/board/outlets/financialReporting/management/styles.ts
@@ -5,6 +5,10 @@ export const ManagementContainer = styled.div`
   display: flex;
   flex-direction: column;
   height: 100%;
+
+  @media (max-width: 768px) {
+    height: 328px;
+  }
 `;
 
 export const ChatContent = styled.div`
