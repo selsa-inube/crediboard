45f62704cef3f9e30818a94261632da09387eeb6
Angelbt12 <angeldavidbaracaldo056@gmail.com>
2024-07-16 12:22:49 -0500

    fix: compatibility settings

--- src/pages/board/outlets/financialReporting/PromissoryNotes/config.tsx
+++ src/pages/board/outlets/financialReporting/PromissoryNotes/config.tsx
@@ -1,6 +1,19 @@
-import { MdOutlineSend, MdOutlineRemoveRedEye } from "react-icons/md";
-import { Icon, Tag } from "@inube/design-system";
+import { isValidElement } from "react";
+import {
+  MdOutlineSend,
+  MdOutlineRemoveRedEye,
+  MdCheck,
+  MdRemove,
+  MdClose,
+} from "react-icons/md";
+import { Icon } from "@inubekit/icon";
+
+
 import { IEntries } from "@components/data/TableBoard/types";
+
+const entrySelection = (data: IEntries) => {
+  console.log(data);
+};
 
 export const titlesFinanacialReporting = [
   {
@@ -19,7 +32,7 @@ export const titlesFinanacialReporting = [
     priority: 3,
   },
   {
-    id: "Estado",
+    id: "tag",
     titleName: "Estado",
     priority: 4,
   },
@@ -31,25 +44,22 @@ export const entriesFinanacialReporting = [
     "No. de Obligación": "1234554545",
     "No. de Documento": "1234567890",
     Tipo: "Pagare",
-    Estado: <Tag label="En tramite" appearance="warning" />,
   },
   {
     id: "2",
     "No. de Obligación": "1234567890",
     "No. de Documento": "1234567890",
     Tipo: "Pagare",
-    Estado: <Tag label="Firmado" appearance="success" />,
   },
   {
     id: "3",
     "No. de Obligación": "1234564321",
     "No. de Documento": "1234567890",
     Tipo: "Libranza",
-    Estado: <Tag label="Con Error" appearance="error" />,
   },
 ];
 
-export const actionsFinanacialReporting = (handleSendButtonClick: (entry: IEntries) => void) => [
+export const actionsFinanacialReporting = [
   {
     id: "Reenviar",
     actionName: "Reenviar",
@@ -57,9 +67,11 @@ export const actionsFinanacialReporting = (handleSendButtonClick: (entry: IEntri
       <Icon
         appearance="primary"
         cursorHover
-        size="24px"
+        size="22px"
+        variant="none"
         icon={<MdOutlineSend />}
-        onClick={() => handleSendButtonClick(data)}
+        onClick={() => entrySelection(data)}
+        spacing="none"
       />
     ),
   },
@@ -69,11 +81,89 @@ export const actionsFinanacialReporting = (handleSendButtonClick: (entry: IEntri
     content: (data: IEntries) => (
       <Icon
         appearance="primary"
-        size="24px"
+        size="22px"
+        spacing="none"
+        variant="none"
         cursorHover
         icon={<MdOutlineRemoveRedEye />}
-        onClick={() => console.log(data)}
+        onClick={() => entrySelection(data)}
       />
     ),
   },
 ];
+
+const iconActionsMobile = (tag: string) => {
+  if (tag === "Aprobado") {
+    return <MdCheck />;
+  } else if (tag === "Pendiente") {
+    return <MdRemove />;
+  } else {
+    return <MdClose />;
+  }
+};
+
+interface TagProps {
+  appearance?: string;
+  label?: string;
+}
+
+interface TagElement {
+  props: TagProps;
+}
+
+const isValidTagElement = (element: unknown): element is TagElement => {
+  return isValidElement(element) && element.props !== undefined;
+};
+
+export const actionMobile = [
+  {
+    id: "tags",
+    actionName: "",
+    content: (data: IEntries) => (
+      <Icon
+        icon={
+          isValidElement(data?.tag) &&
+          iconActionsMobile(data?.tag?.props?.label)
+        }
+        appearance={
+          isValidTagElement(data?.tag)
+            ? data?.tag?.props?.appearance
+            : undefined
+        }
+        spacing="none"
+        cursorHover
+        variant="filled"
+        shape="circle"
+      />
+    ),
+  },
+  {
+    id: "Reenviar",
+    actionName: "Reenviar",
+    content: (data: IEntries) => (
+      <Icon
+        appearance="primary"
+        size="22px"
+        spacing="none"
+        cursorHover
+        icon={<MdOutlineSend />}
+        onClick={() => entrySelection(data)}
+      />
+    ),
+  },
+  {
+    id: "ver imagen",
+    actionName: "Ver Imagen",
+    content: (data: IEntries) => (
+      <Icon
+        appearance="primary"
+        size="22px"
+        spacing="none"
+        cursorHover
+        icon={<MdOutlineRemoveRedEye />}
+        onClick={() => entrySelection(data)}
+      />
+    ),
+  },
+];
+
--- src/pages/board/outlets/financialReporting/PromissoryNotes/index.tsx
+++ src/pages/board/outlets/financialReporting/PromissoryNotes/index.tsx
@@ -1,50 +1,29 @@
-import { useState } from "react";
 import { Stack } from "@inube/design-system";
+
 import { Fieldset } from "@src/components/data/Fieldset";
 import { TableBoard } from "@src/components/data/TableBoard";
 import {
+
   actionsFinanacialReporting,
   entriesFinanacialReporting,
   titlesFinanacialReporting,
 } from "./config";
-import { PromissoryNotesModal } from "@components/modals/PromissoryNotesModal";
 
 export const PromissoryNotes = () => {
-  const [showModal, setShowModal] = useState(false);
-
-  const handleSendButtonClick = () => {
-    setShowModal(true);
-  };
-
-  const handleCloseModal = () => {
-    setShowModal(false);
-  };
-
   return (
     <Stack direction="column">
-      <Fieldset title="Pagarés y Libranzas">
+      <Fieldset title="Pagarés y Libranzas" heigthFieldset="163px">
         <TableBoard
           id="promissoryNotes"
           titles={titlesFinanacialReporting}
           entries={entriesFinanacialReporting}
-          actions={actionsFinanacialReporting(handleSendButtonClick)}
+          actions={actionsFinanacialReporting}
           appearanceTable={{
             efectzebra: true,
             title: "primary",
           }}
         />
       </Fieldset>
-      {showModal && (
-        <PromissoryNotesModal
-          title="Gestor Comercial y Analista"
-          buttonText="Aceptar"
-          onCloseModal={handleCloseModal}
-          onSubmit={(values) => {
-            console.log("Form submitted with values:", values);
-            handleCloseModal();
-          }}
-        />
-      )}
     </Stack>
   );
 };
