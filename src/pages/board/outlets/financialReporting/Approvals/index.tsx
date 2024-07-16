58125ce21404237f19e44b92ded9a9049e435a0e
Andres Babativa <carlos3k11@gmail.com>
2024-07-16 18:08:50 -0500

    refactor: remove the handleData function and unused imports

--- src/pages/board/outlets/financialReporting/Approvals/index.tsx
+++ src/pages/board/outlets/financialReporting/Approvals/index.tsx
@@ -3,13 +3,25 @@ import { useEffect, useState } from "react";
 import { Fieldset } from "@components/data/Fieldset";
 import { TableBoard } from "@components/data/TableBoard";
 import { IEntries } from "@components/data/TableBoard/types";
+import { Tag } from "@components/data/Tag";
 
 import {
   actionMobileApprovals,
   actionsApprovals,
-  handleData,
   titlesApprovals,
 } from "./config";
+import { get } from "@mocks/utils/dataMock.service";
+import { approval_by_credit_request_Mock } from "@mocks/financialReporting/Approvals";
+
+const appearanceTag = (label: string) => {
+  if (label === "Pendiente") {
+    return "warning";
+  }
+  if (label === "Aprobado") {
+    return "success";
+  }
+  return "danger";
+};
 
 export const Approvals = () => {
   const [entriesApprovals, setEntriesApprovals] = useState<IEntries[]>([]);
@@ -17,9 +29,20 @@ export const Approvals = () => {
   const [loading, setLoading] = useState(false);
 
   useEffect(() => {
-    setLoading(true);
-    handleData().then((data) => {
-      setEntriesApprovals(data as IEntries[]);
+    get<typeof approval_by_credit_request_Mock>("approval").then((data) => {
+      setLoading(true);
+      const entries = data.map((entry) => ({
+        id: entry.approval_id.toString(),
+        usuarios: entry.approver_name,
+        error: entry.error,
+        tag: (
+          <Tag
+            label={entry.concept}
+            appearance={appearanceTag(entry.concept)}
+          />
+        ),
+      }));
+      setEntriesApprovals(entries);
       setLoading(false);
     });
   }, []);
