4be36c732c89cbf17edb3b4eb88e06135a797ce4
Andres Babativa <carlos3k11@gmail.com>
2024-07-23 10:32:38 -0500

    feat: add payroll discount authorization mock data

---
+++ src/mocks/promissoryNotes/payrollDiscountAuthorization.mock.ts
@@ -0,0 +1,106 @@
+export const payroll_discount_authorization_signature_by_credit_request = [
+  {
+    payroll_discount_authorization_signature_id:
+      "a00d03c2-078d-4903-a044-44ca0d707cc8",
+    credit_request_id: "999990",
+    description_use: "En tramite",
+    abbreviated_name: "Pagare",
+    credit_product_id: "1234554545",
+  },
+  {
+    payroll_discount_authorization_signature_id:
+      "428274da-01ac-4d9c-beda-3fce4ddcd79b",
+    credit_request_id: "999990",
+    description_use: "Firmado",
+    abbreviated_name: "Pagare",
+    credit_product_id: "1234567890",
+  },
+  {
+    payroll_discount_authorization_signature_id:
+      "9a0e11be-edda-48b5-8677-1b96dc4f4f9c",
+    credit_request_id: "999990",
+    description_use: "Con error",
+    abbreviated_name: "Libranza",
+    credit_product_id: "1234564321",
+  },
+  {
+    payroll_discount_authorization_signature_id:
+      "9a0e11be-edda-48b5-8677-1b96dc4f4f9c",
+    credit_request_id: "999991",
+    description_use: "Con error",
+    abbreviated_name: "Libranza",
+    credit_product_id: "1234564321",
+  },
+  {
+    payroll_discount_authorization_signature_id:
+      "7b1e22df-fggh-59b6-9788-2b97ed5g5g0d",
+    credit_request_id: "999992",
+    description_use: "Firmado",
+    abbreviated_name: "Pagare",
+    credit_product_id: "1234564322",
+  },
+  {
+    payroll_discount_authorization_signature_id:
+      "2c2f33gh-hiij-70c7-1099-3c98fg6h6h1e",
+    credit_request_id: "999993",
+    description_use: "En tramite",
+    abbreviated_name: "Libranza",
+    credit_product_id: "1234564323",
+  },
+  {
+    payroll_discount_authorization_signature_id:
+      "3d3g44ij-ijkl-81d8-2110-4d09gh7i7i2f",
+    credit_request_id: "999994",
+    description_use: "Con error",
+    abbreviated_name: "Pagare",
+    credit_product_id: "1234564324",
+  },
+  {
+    payroll_discount_authorization_signature_id:
+      "4e4h55kl-jklm-92e9-3121-5e10hi8j8j3g",
+    credit_request_id: "999995",
+    description_use: "Firmado",
+    abbreviated_name: "Libranza",
+    credit_product_id: "1234564325",
+  },
+  {
+    payroll_discount_authorization_signature_id:
+      "5f5i66mn-klmn-a3f0-4232-6f21ij9k9k4h",
+    credit_request_id: "999996",
+    description_use: "En tramite",
+    abbreviated_name: "Pagare",
+    credit_product_id: "1234564326",
+  },
+  {
+    payroll_discount_authorization_signature_id:
+      "6g6j77op-lmno-b4g1-5343-7g32jk0l0l5i",
+    credit_request_id: "999997",
+    description_use: "Con error",
+    abbreviated_name: "Libranza",
+    credit_product_id: "1234564327",
+  },
+  {
+    payroll_discount_authorization_signature_id:
+      "7h7k88qr-mnop-c5h2-6454-8h43kl1m1m6j",
+    credit_request_id: "999998",
+    description_use: "Firmado",
+    abbreviated_name: "Pagare",
+    credit_product_id: "1234564328",
+  },
+  {
+    payroll_discount_authorization_signature_id:
+      "8i8l99st-opqr-d6i3-7565-9i54lm2n2n7k",
+    credit_request_id: "999998",
+    description_use: "En tramite",
+    abbreviated_name: "Libranza",
+    credit_product_id: "1234564329",
+  },
+  {
+    payroll_discount_authorization_signature_id:
+      "9j9m00uv-pqrs-e7j4-8676-aj65mn3o3o8l",
+    credit_request_id: "999999",
+    description_use: "Con error",
+    abbreviated_name: "Pagare",
+    credit_product_id: "1234564330",
+  },
+];
