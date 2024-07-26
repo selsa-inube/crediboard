b72ef792a466c8a99f946054f92fe9b3e331019a
Andres Babativa <carlos3k11@gmail.com>
2024-07-26 10:47:10 -0500

    feat(FinancialReporting): remove unused props and import statements

---
+++ src/pages/board/outlets/financialReporting/Approvals/types.ts
@@ -0,0 +1,8 @@
+export interface approval_by_credit_request_Mock {
+  approval_id: string;
+  credit_request_id: string;
+  approver_id: string;
+  approver_name: string;
+  concept: string;
+  error: boolean;
+}
