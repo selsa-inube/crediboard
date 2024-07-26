cb572ab3833def23ad6519d4a105a542dc3b1643
Angelbt12 <angeldavidbaracaldo056@gmail.com>
2024-07-26 18:36:14 -0500

    fix: export adjustment

--- src/pages/board/outlets/financialReporting/PromissoryNotes/config.tsx
+++ src/pages/board/outlets/financialReporting/PromissoryNotes/config.tsx
@@ -7,7 +7,7 @@ import {
   MdClose,
 } from "react-icons/md";
 import { Icon } from "@inubekit/icon";
-import { Tag } from "@inubekit/tag";
+import { Tag } from "@components/data/Tag";
 
 import { IEntries } from "@components/data/TableBoard/types";
 
