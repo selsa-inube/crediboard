cfb6fef44bff80bc96365e7323762d7fe51316bd
carin <ca.rincon97@gmail.com>
2024-07-29 15:06:50 -0500

    fix: deleting comments

--- src/utils/pdf/generetePDF.ts
+++ src/utils/pdf/generetePDF.ts
@@ -1,49 +1,5 @@
 import jsPDF from "jspdf";
 import html2canvas from "html2canvas";
-/* 
-export const generatePDF = (
-  elementPrint: React.RefObject<HTMLDivElement>,
-
-  titlePDF = "document"
-) => {
-  if (elementPrint.current === null) return;
-
-  const pdf = new jsPDF({ orientation: "landscape", format: "" });
-  const margin = 10;
-  const titleFontSize = 16;
-  const scale = 1.5;
-
-  const options = {
-    scale,
-    useCORS: true,
-  };
-
-  html2canvas(elementPrint.current, options)
-    .then((canvas) => {
-      const imgData = canvas.toDataURL("image/png");
-      const pageWidth = pdf.internal.pageSize.getWidth();
-      const pageHeight = pdf.internal.pageSize.getHeight();
-      const imgWidth = pageWidth - margin * 2;
-      const imgHeight = (canvas.height * imgWidth) / canvas.width;
-      let heightLeft = imgHeight;
-      let position = margin + titleFontSize + 10;
-
-      while (heightLeft >= 0) {
-        pdf.addImage(imgData, "PNG", margin, position, imgWidth, imgHeight);
-        heightLeft -= pageHeight - position;
-        if (heightLeft >= 0) {
-          pdf.addPage();
-          position = margin;
-        }
-      }
-
-      pdf.save(titlePDF);
-    })
-    .catch((error) => {
-      console.error("Error al generar el PDF:", error);
-    });
-};
- */
 
 export const generatePDF = (
   elementPrint: React.RefObject<HTMLDivElement>,
