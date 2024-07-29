2f6b0a5b874175fe0c4d77dc945230e3a6282ff2
Andres Babativa <carlos3k11@gmail.com>
2024-07-29 18:13:11 -0500

    feat: update PromissoryNotes component to fetch and display data

--- src/pages/board/outlets/financialReporting/PromissoryNotes/index.tsx
+++ src/pages/board/outlets/financialReporting/PromissoryNotes/index.tsx
@@ -1,20 +1,60 @@
-import { Stack, useMediaQuery} from "@inube/design-system";
+import { useEffect, useState } from "react";
+import { Stack, useMediaQuery } from "@inube/design-system";
 
-
-import { Fieldset } from "@src/components/data/Fieldset";
-import { TableBoard } from "@src/components/data/TableBoard";
+import { Fieldset } from "@components/data/Fieldset";
+import { TableBoard } from "@components/data/TableBoard";
 import {
   actionMobile,
   actionsFinanacialReporting,
-  entriesFinanacialReporting,
   titlesFinanacialReporting,
 } from "./config";
-
-
+import { payroll_discount_authorization, promissory_note } from "./types";
+import { getDataById } from "@mocks/utils/dataMock.service";
+import { useParams } from "react-router-dom";
+import { Tag } from "@components/data/Tag";
+import { IEntries } from "@components/data/TableBoard/types";
 
 export const PromissoryNotes = () => {
+  const { id } = useParams();
+
+  const [dataPromissoryNotes, setDataPromissoryNotes] = useState<IEntries[]>(
+    []
+  );
+
+  useEffect(() => {
+    Promise.allSettled([
+      getDataById<payroll_discount_authorization[]>(
+        "payroll_discount_authorization",
+        "credit_request_id",
+        id!
+      ),
+      getDataById<promissory_note[]>(
+        "promissory_note",
+        "credit_request_id",
+        id!
+      ),
+    ]).then((results) => {
+      const data = results
+        .flatMap((client): payroll_discount_authorization[] => {
+          if (client.status === "fulfilled") {
+            return client.value as payroll_discount_authorization[];
+          }
+          return [];
+        })
+        .map((p) => ({
+          id: p.credit_product_id,
+          "No. de Obligación": p.obligation_unique_code,
+          "No. de Documento": p.document_unique_code,
+          Tipo: p.state,
+          tag: <Tag label={p.state} appearance="success" />,
+        }));
+      setDataPromissoryNotes(data);
+    });
+  }, [id]);
 
   const isMobile = useMediaQuery("(max-width: 720px)");
+
+  console.log("dataPromissoryNotes", dataPromissoryNotes);
 
   return (
     <Stack direction="column">
@@ -22,7 +62,7 @@ export const PromissoryNotes = () => {
         <TableBoard
           id="promissoryNotes"
           titles={titlesFinanacialReporting}
-          entries={entriesFinanacialReporting}
+          entries={dataPromissoryNotes}
           actions={actionsFinanacialReporting}
           actionMobile={actionMobile}
           appearanceTable={{
