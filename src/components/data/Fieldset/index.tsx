d899c0d9224fb144abfb9a22323097bc5f39e007
Angelbt12 <angeldavidbaracaldo056@gmail.com>
2024-08-02 08:34:07 -0500

    fix: update to inubekid

--- src/components/data/Fieldset/index.tsx
+++ src/components/data/Fieldset/index.tsx
@@ -1,11 +1,10 @@
 import { MdAdd } from "react-icons/md";
 import {
-  Button,
-  Stack,
-  Text,
-  inube,
   useMediaQuery,
 } from "@inube/design-system";
+import { Text } from "@inubekit/text";
+import { Stack } from "@inubekit/stack";
+import { Button } from "@inubekit/button";
 import { StyledContainerFieldset } from "./styles";
 
 interface IPtionsButton {
@@ -41,12 +40,12 @@ export const Fieldset = (props: IFieldsetProps) => {
   return (
     <Stack
       direction="column"
-      gap={inube.spacing.s100}
+      gap="8px"
       width="-webkit-fill-available"
       height={!isMobile ? heightFieldset : "auto"}
     >
       <Stack justifyContent={activeButton && "space-between"}>
-        <Stack gap={isMobile ? inube.spacing.s150 : inube.spacing.s100}>
+        <Stack gap={isMobile ? "12px" : "8px"}>
           <Text
             type="title"
             appearance="gray"
