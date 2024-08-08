c1e6c066a03a557cf0e4d0dfe0ad2ceaa95f68ec
Andres Babativa <carlos3k11@gmail.com>
2024-08-08 18:23:18 -0500

    feat: reate a DB mock for job stability

---
+++ src/mocks/financialReporting/jobStability.mock.ts
@@ -0,0 +1,370 @@
+export const job_stability = [
+  {
+    credit_request_id: "999990",
+    job_stability: {
+      estabilidad_laboral: {
+        antiguedad_en_la_empresa: "5 años",
+        indice_estabilidad_laboral: 900,
+        max_indice_estabilidad_laboral: 1000,
+        indemnizacion_estimada: 20000000,
+      },
+      capacidad_de_pago: {
+        valor_disponible: 955320,
+        porcentaje_disponible: 32,
+        ingreso_base: 3000000,
+        porcentaje_usado: 68,
+      },
+      cartera_descubierta: {
+        factor_descubierto: 10,
+        valor_descubierto: 50000000,
+        reciprocidad: 5,
+        dep_permanentes: "Dep. Permanentes",
+      },
+      scoring_de_riesgo: {
+        puntaje_total: 456,
+        puntaje_minimo: 500,
+        detalle_puntaje: {
+          antiguedad_10_anos: 120,
+          central_de_riesgo: -100,
+          indice_estabilidad_laboral: 300,
+          estado_civil_casado: 50,
+        },
+      },
+      garantias: {
+        requeridas: "Ninguna garantía real, o fianza o codeudor.",
+        ofrecidas:
+          "Ninguna, casa Bogotá 200 mt2, o fianza o codeudor Pedro Pérez.",
+        vigentes:
+          "Ninguna, apartamento, en Bogotá 80 mt2, o vehículo Marzo 2023.",
+      },
+      comportamiento_crediticio: {
+        score_central_de_riesgo: 250,
+        fecha_score: "Agosto 31 / 2023",
+        numero_moras: 9,
+        max_numero_cuotas_mora: 3,
+      },
+    },
+  },
+  {
+    credit_request_id: "999991",
+    job_stability: {
+      estabilidad_laboral: {
+        antiguedad_en_la_empresa: "5 años",
+        indice_estabilidad_laboral: 900,
+        max_indice_estabilidad_laboral: 1000,
+        indemnizacion_estimada: 20000000,
+      },
+      capacidad_de_pago: {
+        valor_disponible: 955320,
+        porcentaje_disponible: 32,
+        ingreso_base: 3000000,
+        porcentaje_usado: 68,
+      },
+      cartera_descubierta: {
+        factor_descubierto: 10,
+        valor_descubierto: 50000000,
+        reciprocidad: 5,
+        dep_permanentes: "Dep. Permanentes",
+      },
+      scoring_de_riesgo: {
+        puntaje_total: 456,
+        puntaje_minimo: 500,
+        detalle_puntaje: {
+          antiguedad_10_anos: 120,
+          central_de_riesgo: -100,
+          indice_estabilidad_laboral: 300,
+          estado_civil_casado: 50,
+        },
+      },
+      garantias: {
+        requeridas: "Ninguna garantía real, o fianza o codeudor.",
+        ofrecidas:
+          "Ninguna, casa Bogotá 200 mt2, o fianza o codeudor Pedro Pérez.",
+        vigentes:
+          "Ninguna, apartamento, en Bogotá 80 mt2, o vehículo Marzo 2023.",
+      },
+      comportamiento_crediticio: {
+        score_central_de_riesgo: 250,
+        fecha_score: "Agosto 31 / 2023",
+        numero_moras: 9,
+        max_numero_cuotas_mora: 3,
+      },
+    },
+  },
+  {
+    credit_request_id: "999992",
+    job_stability: {
+      estabilidad_laboral: {
+        antiguedad_en_la_empresa: "5 años",
+        indice_estabilidad_laboral: 900,
+        max_indice_estabilidad_laboral: 1000,
+        indemnizacion_estimada: 20000000,
+      },
+      capacidad_de_pago: {
+        valor_disponible: 955320,
+        porcentaje_disponible: 32,
+        ingreso_base: 3000000,
+        porcentaje_usado: 68,
+      },
+      cartera_descubierta: {
+        factor_descubierto: 10,
+        valor_descubierto: 50000000,
+        reciprocidad: 5,
+        dep_permanentes: "Dep. Permanentes",
+      },
+      scoring_de_riesgo: {
+        puntaje_total: 456,
+        puntaje_minimo: 500,
+        detalle_puntaje: {
+          antiguedad_10_anos: 120,
+          central_de_riesgo: -100,
+          indice_estabilidad_laboral: 300,
+          estado_civil_casado: 50,
+        },
+      },
+      garantias: {
+        requeridas: "Ninguna garantía real, o fianza o codeudor.",
+        ofrecidas:
+          "Ninguna, casa Bogotá 200 mt2, o fianza o codeudor Pedro Pérez.",
+        vigentes:
+          "Ninguna, apartamento, en Bogotá 80 mt2, o vehículo Marzo 2023.",
+      },
+      comportamiento_crediticio: {
+        score_central_de_riesgo: 250,
+        fecha_score: "Agosto 31 / 2023",
+        numero_moras: 9,
+        max_numero_cuotas_mora: 3,
+      },
+    },
+  },
+  {
+    credit_request_id: "999993",
+    job_stability: {
+      estabilidad_laboral: {
+        antiguedad_en_la_empresa: "5 años",
+        indice_estabilidad_laboral: 900,
+        max_indice_estabilidad_laboral: 1000,
+        indemnizacion_estimada: 20000000,
+      },
+      capacidad_de_pago: {
+        valor_disponible: 955320,
+        porcentaje_disponible: 32,
+        ingreso_base: 3000000,
+        porcentaje_usado: 68,
+      },
+      cartera_descubierta: {
+        factor_descubierto: 10,
+        valor_descubierto: 50000000,
+        reciprocidad: 5,
+        dep_permanentes: "Dep. Permanentes",
+      },
+      scoring_de_riesgo: {
+        puntaje_total: 456,
+        puntaje_minimo: 500,
+        detalle_puntaje: {
+          antiguedad_10_anos: 120,
+          central_de_riesgo: -100,
+          indice_estabilidad_laboral: 300,
+          estado_civil_casado: 50,
+        },
+      },
+      garantias: {
+        requeridas: "Ninguna garantía real, o fianza o codeudor.",
+        ofrecidas:
+          "Ninguna, casa Bogotá 200 mt2, o fianza o codeudor Pedro Pérez.",
+        vigentes:
+          "Ninguna, apartamento, en Bogotá 80 mt2, o vehículo Marzo 2023.",
+      },
+      comportamiento_crediticio: {
+        score_central_de_riesgo: 250,
+        fecha_score: "Agosto 31 / 2023",
+        numero_moras: 9,
+        max_numero_cuotas_mora: 3,
+      },
+    },
+  },
+  {
+    credit_request_id: "999994",
+    job_stability: {
+      estabilidad_laboral: {
+        antiguedad_en_la_empresa: "5 años",
+        indice_estabilidad_laboral: 900,
+        max_indice_estabilidad_laboral: 1000,
+        indemnizacion_estimada: 20000000,
+      },
+      capacidad_de_pago: {
+        valor_disponible: 955320,
+        porcentaje_disponible: 32,
+        ingreso_base: 3000000,
+        porcentaje_usado: 68,
+      },
+      cartera_descubierta: {
+        factor_descubierto: 10,
+        valor_descubierto: 50000000,
+        reciprocidad: 5,
+        dep_permanentes: "Dep. Permanentes",
+      },
+      scoring_de_riesgo: {
+        puntaje_total: 456,
+        puntaje_minimo: 500,
+        detalle_puntaje: {
+          antiguedad_10_anos: 120,
+          central_de_riesgo: -100,
+          indice_estabilidad_laboral: 300,
+          estado_civil_casado: 50,
+        },
+      },
+      garantias: {
+        requeridas: "Ninguna garantía real, o fianza o codeudor.",
+        ofrecidas:
+          "Ninguna, casa Bogotá 200 mt2, o fianza o codeudor Pedro Pérez.",
+        vigentes:
+          "Ninguna, apartamento, en Bogotá 80 mt2, o vehículo Marzo 2023.",
+      },
+      comportamiento_crediticio: {
+        score_central_de_riesgo: 250,
+        fecha_score: "Agosto 31 / 2023",
+        numero_moras: 9,
+        max_numero_cuotas_mora: 3,
+      },
+    },
+  },
+  {
+    credit_request_id: "999995",
+    job_stability: {
+      estabilidad_laboral: {
+        antiguedad_en_la_empresa: "5 años",
+        indice_estabilidad_laboral: 900,
+        max_indice_estabilidad_laboral: 1000,
+        indemnizacion_estimada: 20000000,
+      },
+      capacidad_de_pago: {
+        valor_disponible: 955320,
+        porcentaje_disponible: 32,
+        ingreso_base: 3000000,
+        porcentaje_usado: 68,
+      },
+      cartera_descubierta: {
+        factor_descubierto: 10,
+        valor_descubierto: 50000000,
+        reciprocidad: 5,
+        dep_permanentes: "Dep. Permanentes",
+      },
+      scoring_de_riesgo: {
+        puntaje_total: 456,
+        puntaje_minimo: 500,
+        detalle_puntaje: {
+          antiguedad_10_anos: 120,
+          central_de_riesgo: -100,
+          indice_estabilidad_laboral: 300,
+          estado_civil_casado: 50,
+        },
+      },
+      garantias: {
+        requeridas: "Ninguna garantía real, o fianza o codeudor.",
+        ofrecidas:
+          "Ninguna, casa Bogotá 200 mt2, o fianza o codeudor Pedro Pérez.",
+        vigentes:
+          "Ninguna, apartamento, en Bogotá 80 mt2, o vehículo Marzo 2023.",
+      },
+      comportamiento_crediticio: {
+        score_central_de_riesgo: 250,
+        fecha_score: "Agosto 31 / 2023",
+        numero_moras: 9,
+        max_numero_cuotas_mora: 3,
+      },
+    },
+  },
+  {
+    credit_request_id: "999996",
+    job_stability: {
+      estabilidad_laboral: {
+        antiguedad_en_la_empresa: "5 años",
+        indice_estabilidad_laboral: 900,
+        max_indice_estabilidad_laboral: 1000,
+        indemnizacion_estimada: 20000000,
+      },
+      capacidad_de_pago: {
+        valor_disponible: 955320,
+        porcentaje_disponible: 32,
+        ingreso_base: 3000000,
+        porcentaje_usado: 68,
+      },
+      cartera_descubierta: {
+        factor_descubierto: 10,
+        valor_descubierto: 50000000,
+        reciprocidad: 5,
+        dep_permanentes: "Dep. Permanentes",
+      },
+      scoring_de_riesgo: {
+        puntaje_total: 456,
+        puntaje_minimo: 500,
+        detalle_puntaje: {
+          antiguedad_10_anos: 120,
+          central_de_riesgo: -100,
+          indice_estabilidad_laboral: 300,
+          estado_civil_casado: 50,
+        },
+      },
+      garantias: {
+        requeridas: "Ninguna garantía real, o fianza o codeudor.",
+        ofrecidas:
+          "Ninguna, casa Bogotá 200 mt2, o fianza o codeudor Pedro Pérez.",
+        vigentes:
+          "Ninguna, apartamento, en Bogotá 80 mt2, o vehículo Marzo 2023.",
+      },
+      comportamiento_crediticio: {
+        score_central_de_riesgo: 250,
+        fecha_score: "Agosto 31 / 2023",
+        numero_moras: 9,
+        max_numero_cuotas_mora: 3,
+      },
+    },
+  },
+  {
+    credit_request_id: "999997",
+    job_stability: {
+      estabilidad_laboral: {
+        antiguedad_en_la_empresa: "5 años",
+        indice_estabilidad_laboral: 900,
+        max_indice_estabilidad_laboral: 1000,
+        indemnizacion_estimada: 20000000,
+      },
+      capacidad_de_pago: {
+        valor_disponible: 955320,
+        porcentaje_disponible: 32,
+        ingreso_base: 3000000,
+        porcentaje_usado: 68,
+      },
+      cartera_descubierta: {
+        factor_descubierto: 10,
+        valor_descubierto: 50000000,
+        reciprocidad: 5,
+        dep_permanentes: "Dep. Permanentes",
+      },
+      scoring_de_riesgo: {
+        puntaje_total: 456,
+        puntaje_minimo: 500,
+        detalle_puntaje: {
+          antiguedad_10_anos: 120,
+          central_de_riesgo: -100,
+          indice_estabilidad_laboral: 300,
+          estado_civil_casado: 50,
+        },
+      },
+      garantias: {
+        requeridas: "Ninguna garantía real, o fianza o codeudor.",
+        ofrecidas:
+          "Ninguna, casa Bogotá 200 mt2, o fianza o codeudor Pedro Pérez.",
+        vigentes:
+          "Ninguna, apartamento, en Bogotá 80 mt2, o vehículo Marzo 2023.",
+      },
+      comportamiento_crediticio: {
+        score_central_de_riesgo: 250,
+        fecha_score: "Agosto 31 / 2023",
+        numero_moras: 9,
+        max_numero_cuotas_mora: 3,
+      },
+    },
+  },
+];
