36eccb6ef70675808562357c2d6bdd73ba25ae7c
Andres Babativa <carlos3k11@gmail.com>
2024-08-13 17:54:46 -0500

    feat(AppPage): create a nav for Rules the options presented in this nav

--- src/components/layout/AppPage/config/apps.config.tsx
+++ src/components/layout/AppPage/config/apps.config.tsx
@@ -1,4 +1,10 @@
-import { MdVpnKey } from "react-icons/md";
+import {
+  MdKey,
+  MdMeetingRoom,
+  MdPhone,
+  MdStarBorder,
+  MdVpnKey,
+} from "react-icons/md";
 
 const appsConfig = [
   {
@@ -43,6 +49,47 @@ const navigationConfig = {
 const logoutConfig = {
   logoutPath: "/logout",
   logoutTitle: "Cerrar sesión",
+};
+
+export const navigationMock = {
+  title: "REGLAS",
+  sections: {
+    rules: {
+      name: "Unidad de negocio",
+      links: {
+        Crediboart: {
+          id: "Crediboart",
+          label: "Crediboart",
+          icon: <MdKey />,
+          path: "/components/text",
+        },
+        Presente: {
+          id: "Presente",
+          label: "Presente",
+          icon: <MdMeetingRoom />,
+          path: "/components/textfield",
+        },
+        Cooservunal: {
+          id: "Cooservunal",
+          label: "Cooservunal",
+          icon: <MdPhone />,
+          path: "/components/textarea",
+        },
+        Corbanca: {
+          id: "Corbanca",
+          label: "Corbanca",
+          icon: <MdStarBorder />,
+          path: "/crm",
+        },
+        Fedecom: {
+          id: "Fedecom",
+          label: "Fedecom",
+          icon: <MdStarBorder />,
+          path: "/crm",
+        },
+      },
+    },
+  },
 };
 
 export { appsConfig, navigationConfig, logoutConfig };
--- src/components/layout/AppPage/index.tsx
+++ src/components/layout/AppPage/index.tsx
@@ -1,14 +1,19 @@
 import { useContext, useState, useRef, useEffect } from "react";
 import { Outlet } from "react-router-dom";
-import { Header, Grid } from "@inube/design-system";
+import { Header, Grid, useMediaQuery } from "@inube/design-system";
 import { MdLogout } from "react-icons/md";
+import { Nav } from "@inubekit/nav";
 
 import { AppContext } from "@context/AppContext";
 import { MenuSection } from "@components/navigation/MenuSection";
 import { MenuUser } from "@components/navigation/MenuUser";
 import { LogoutModal } from "@components/feedback/LogoutModal";
 
-import { navigationConfig, logoutConfig } from "./config/apps.config";
+import {
+  navigationConfig,
+  logoutConfig,
+  navigationMock,
+} from "./config/apps.config";
 import {
   StyledAppPage,
   StyledContainer,
@@ -59,6 +64,8 @@ function AppPage() {
     setShowUserMenu(false);
   };
 
+  const smallScreen = useMediaQuery("(max-width: 849px)");
+
   const sections = [
     {
       links: [
@@ -81,6 +88,64 @@ function AppPage() {
           userName={user.username}
           client={user.company}
         />
+        {showUserMenu && (
+          <StyledMenuContainer ref={userMenuRef}>
+            <MenuUser userName={user.username} businessUnit={user.company} />
+            <MenuSection sections={sections} divider={true} />
+          </StyledMenuContainer>
+        )}
+        {showLogoutModal && (
+          <LogoutModal
+            logoutPath={logoutConfig.logoutPath}
+            handleShowBlanket={handleToggleLogoutModal}
+          />
+        )}
+        <StyledContainer>
+          <Grid
+            templateColumns={smallScreen ? "1fr" : "auto 1fr"}
+            alignContent="unset"
+          >
+            {!smallScreen && <Nav navigation={navigationMock} />}
+
+            <StyledMain>
+              <Outlet />
+            </StyledMain>
+          </Grid>
+        </StyledContainer>
+      </Grid>
+    </StyledAppPage>
+  );
+}
+
+export { AppPage };
+
+/* 
+
+
+ <StyledContainer>
+          <Grid
+            templateColumns={smallScreen ? "1fr" : "auto 1fr"}
+            alignContent="unset"
+          >
+            {!smallScreen && (
+              <StyledContainerNav>
+                <Nav
+                  navigation={navigationConfig}
+                  logoutPath={logoutConfig.logoutPath}
+                  logoutTitle={logoutConfig.logoutTitle}
+                />
+              </StyledContainerNav>
+            )}
+
+            <StyledMain>
+              <Outlet />
+            </StyledMain>
+          </Grid>
+        </StyledContainer>
+
+
+
+<Nav navigation={navigationMock} />
         <StyledContainer>
           {showUserMenu && (
             <StyledMenuContainer ref={userMenuRef}>
@@ -98,9 +163,4 @@ function AppPage() {
             <Outlet />
           </StyledMain>
         </StyledContainer>
-      </Grid>
-    </StyledAppPage>
-  );
-}
-
-export { AppPage };
+*/
