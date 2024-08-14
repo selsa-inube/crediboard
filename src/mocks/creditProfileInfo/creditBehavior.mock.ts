d6fc854793b36c72348f2c9451dccade5845dda0
Angelbt12 <angeldavidbaracaldo056@gmail.com>
2024-08-14 09:06:48 -0500

    fix: mock fit

--- src/mocks/creditProfileInfo/creditBehavior.mock.ts
+++ src/mocks/creditProfileInfo/creditBehavior.mock.ts
@@ -1,64 +1,72 @@
 export const credit_behaviorInfo = [
   {
     credit_request_id: "999990",
-    Credit_behavior: {
+    credit_behavior: {
         core_risk_score: 250 ,
+        central_risk_scor_date: "ago/31/2023",
         number_of_internal_arrears: 9,
         maximum_number_of_installments_in_arrears: 3,
     }
   },
   {
     credit_request_id: "999991",
-    Credit_behavior: {
+    credit_behavior: {
         core_risk_score: 200 ,
+        central_risk_scor_date: "ene/31/2023",
         number_of_internal_arrears: 5,
         maximum_number_of_installments_in_arrears: 3,
     }
   },
   {
     credit_request_id: "999992",
-    Credit_behavior: {
+    credit_behavior: {
         core_risk_score: 255 ,
+        central_risk_scor_date: "jul/31/2023",
         number_of_internal_arrears: 7,
         maximum_number_of_installments_in_arrears: 4,
     }
   },
   {
     credit_request_id: "999993",
-    Credit_behavior: {
+    credit_behavior: {
         core_risk_score: 280 ,
+        central_risk_scor_date: "jun/31/2023",
         number_of_internal_arrears: 10,
         maximum_number_of_installments_in_arrears: 5,
     }
   },
   {
     credit_request_id: "999994",
-    Credit_behavior: {
+    credit_behavior: {
         core_risk_score: 230 ,
+        central_risk_scor_date: "dic/31/2023",
         number_of_internal_arrears: 2,
         maximum_number_of_installments_in_arrears: 2,
     }
   },
   {
     credit_request_id: "999995",
-    Credit_behavior: {
+    credit_behavior: {
         core_risk_score: 210 ,
+        central_risk_scor_date: "feb/31/2023",
         number_of_internal_arrears: 3,
         maximum_number_of_installments_in_arrears: 4,
     }
   },
   {
     credit_request_id: "999996",
-    Credit_behavior: {
+    credit_behavior: {
         core_risk_score: 215 ,
+        central_risk_scor_date: "ago/31/2023",
         number_of_internal_arrears: 4,
         maximum_number_of_installments_in_arrears: 1,
     }
   },
   {
     credit_request_id: "999997",
-    Credit_behavior: {
+    credit_behavior: {
         core_risk_score: 217 ,
+        central_risk_scor_date: "may/31/2023",
         number_of_internal_arrears: 5,
         maximum_number_of_installments_in_arrears: 7,
     }
