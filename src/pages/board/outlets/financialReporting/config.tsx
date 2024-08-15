026316295f49070ac1a3c9938e53cd85f22b1673
Andres Babativa <carlos3k11@gmail.com>
2024-08-15 11:46:32 -0500

    refactor: add async/await to handleConfirmReject function

--- src/pages/board/outlets/financialReporting/config.tsx
+++ src/pages/board/outlets/financialReporting/config.tsx
@@ -3,10 +3,10 @@ import { MdAddCircleOutline } from "react-icons/md";
 import { IOptionButtons } from "@components/modals/ListModal";
 import { addItem } from "@src/mocks/utils/dataMock.service";
 
-export const handleConfirmReject = (
+export const handleConfirmReject = async (
   id: string,
   user: string,
-  values: { textarea: string },
+  formData: { textarea: string },
   setFlagMessage: (message: {
     title: string;
     description: string;
@@ -15,40 +15,51 @@ export const handleConfirmReject = (
   setShowFlagMessage: (state: boolean) => void,
   setShowRejectModal: (state: boolean) => void
 ) => {
-  const text = values.textarea;
+  const justificationText = formData.textarea;
 
-  console.log("iser :", user);
+  console.log("user:", user);
 
-  if (text && id) {
+  if (justificationText && id) {
     const trace = {
-      trace_id: crypto.randomUUID(),
-      trace_value: "Document uploaded",
-      credit_request_id: id,
-      use_case: "document_upload",
-      user_id: user,
-      execution_date: new Date().toLocaleDateString(),
-      justification: text,
-      decision_taken_by_user: "rejected",
-      trace_type: "novelty_document",
-      read_novelty: "N",
+      traceId: crypto.randomUUID(),
+      traceValue: "Document uploaded",
+      creditRequestId: id,
+      useCase: "document_upload",
+      userId: user,
+      executionDate: new Date().toLocaleDateString(),
+      justification: justificationText,
+      decisionTakenByUser: "rejected",
+      traceType: "novelty_document",
+      readNovelty: "N",
     };
 
-    addItem("trace", trace).then((res) => {
-      console.log("res", res);
-    });
-  }
+    const handleSuccess = () => {
+      setFlagMessage({
+        title: "Rechazo Confirmado",
+        description: "El rechazo se ha realizado correctamente",
+        appearance: "success",
+      });
+      setShowFlagMessage(true);
+      setShowRejectModal(false);
+    };
 
-  if (text) {
-    console.log("Rechazo Confirmado", id);
-    setFlagMessage({
-      title: "Rechazo Confirmado",
-      description: "El rechazo se ha realizado correctamente",
-      appearance: "success",
-    });
-  }
+    const handleError = (error: Error) => {
+      setFlagMessage({
+        title: "Rechazo Fallido",
+        description: `No se ha podido realizar el rechazo: ${error}`,
+        appearance: "danger",
+      });
+      setShowFlagMessage(true);
+      setShowRejectModal(false);
+    };
 
-  setShowFlagMessage(true);
-  setShowRejectModal(false);
+    try {
+      await addItem("trace", trace);
+      handleSuccess();
+    } catch (error) {
+      handleError(error as Error);
+    }
+  }
 };
 
 export const handleConfirmCancel = (
