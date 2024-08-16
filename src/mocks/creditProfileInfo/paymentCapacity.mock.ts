ca918dc440f1013ff80d74de05a566cb03dac494
Angelbt12 <angeldavidbaracaldo056@gmail.com>
2024-08-16 17:32:41 -0500

    fix: mock settings

--- src/mocks/creditProfileInfo/paymentCapacity.mock.ts
+++ src/mocks/creditProfileInfo/paymentCapacity.mock.ts
@@ -3,72 +3,63 @@ export const payment_capacity = [
     credit_request_id: "999990",
     payment_capacity: {
       available_value: 955500,
-      available_percentage: 20,
       base_income: 3000000,
-      percentage_used: 100 - 20,
-    },
+      percentage_used: 50},
   },
   {
     credit_request_id: "999991",
     payment_capacity: {
       available_value: 995320,
-      available_percentage: 10,
       base_income: 3000000,
-      percentage_used: 100 - 10,
+      percentage_used: 100,
     },
   },
   {
     credit_request_id: "999992",
     payment_capacity: {
       available_value: 944320,
-      available_percentage: 40,
       base_income: 3000000,
-      percentage_used: 100 - 40,
+      percentage_used: 80,
     },
   },
   {
     credit_request_id: "999993",
     payment_capacity: {
       available_value: 933320,
-      available_percentage: 15,
       base_income: 3000000,
-      percentage_used: 100 - 15,
+      percentage_used: 40,
     },
   },
   {
     credit_request_id: "999994",
     payment_capacity: {
       available_value: 966320,
-      available_percentage: 31,
       base_income: 3000000,
-      percentage_used: 100 - 31,
+      percentage_used: 45,
     },
   },
   {
     credit_request_id: "999995",
     payment_capacity: {
       available_value: 855320,
-      available_percentage: 32,
       base_income: 3000000,
-      percentage_used: 100 - 32,
+      percentage_used: 98,
     },
   },
   {
     credit_request_id: "999996",
     payment_capacity: {
       available_value: 755320,
-      available_percentage: 33,
       base_income: 3000000,
-      percentage_used: 100 - 33,
+      percentage_used: 80,
     },
   },
   {
     credit_request_id: "999997",
     payment_capacity: {
       available_value: 988320,
-      available_percentage: 37,
       base_income: 3000000,
-      percentage_used: 100 - 37,
+      percentage_used: 10,
     },
   },
 ];
