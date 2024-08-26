174d2a42290b948ad96f469ddbf652184e21e5f6
Angelbt12 <angeldavidbaracaldo056@gmail.com>
2024-08-26 08:23:19 -0500

    fix: mock implementation

--- src/pages/board/outlets/creditProfileInfo/index.tsx
+++ src/pages/board/outlets/creditProfileInfo/index.tsx
@@ -10,7 +10,7 @@ import { Text } from "@inubekit/text";
 import { useMediaQueries } from "@inubekit/hooks";
 
 import { get, getById, getDataById } from "@mocks/utils/dataMock.service";
-import { Requests, IRiskScoring, credit, Ipayment_capacity } from "@services/types";
+import { Requests, IRiskScoring, credit, Ipayment_capacity, Icredit_behavior } from "@services/types";
 import { capitalizeFirstLetterEachWord } from "@utils/formatData/text";
 import { currencyFormat } from "@utils/formatData/currency";
 import { generatePDF } from "@utils/pdf/generetePDF";
@@ -45,9 +45,15 @@ export const CreditProfileInfo = () => {
     estimated_severance: 0,
   });
   const [payment_capacity, setPayment_capacity] = useState({
-        available_value: 0,
-        base_income: 0,
-        percentage_used: 0,
+    available_value: 0,
+    base_income: 0,
+    percentage_used: 0,
+  });
+  const [credit_behavior, setCredit_behavior] = useState({
+    core_risk_score: 0,
+    central_risk_score_date: 0,
+    number_of_internal_arrears: 0,
+    maximum_number_of_installments_in_arrears: 0,
   });
 
   const [loading, setLoading] = useState(false);
@@ -66,9 +72,10 @@ export const CreditProfileInfo = () => {
       get("risk-scoring"),
       getDataById<credit[]>("credit_profileInfo", "credit_request_id", id!),
       getDataById<Ipayment_capacity[]>("payment_capacity", "credit_request_id", id!),
+      getDataById<Icredit_behavior[]>("credit_behavior", "credit_request_id", id!),
 
     ]).then((data) => {
-      const [request, riskScoring, credit_profileInfo, payment_capacity] = data;
+      const [request, riskScoring, credit_profileInfo, payment_capacity, credit_behavior] = data;
 
       if (request.status === "fulfilled") {
         setRequests(request.value as Requests);
@@ -85,19 +92,23 @@ export const CreditProfileInfo = () => {
         }));
       }
       if (payment_capacity.status === "fulfilled" && payment_capacity.value){       
-        console.log (payment_capacity.status);
         setPayment_capacity((prevState) => ({
           ...prevState,
           ...payment_capacity.value?.[0]?.payment_capacity 
         })); 
-        
       }
-      
+      if (credit_behavior.status === "fulfilled" && credit_behavior.value){
+        setCredit_behavior((prevState) => ({
+          ...prevState,
+          ...credit_behavior.value?.[0]?.credit_behavior
+        }));
+      }
+
+
       setLoading(false);
     });
   }, [id]);
 
-  console.log (payment_capacity);
 
   const handlePrint = () => {
     setIsGeneratingPdf(true);
@@ -255,10 +266,10 @@ export const CreditProfileInfo = () => {
           isMobile={isMobile}
         />
         <CreditBehavior
-          centralScoreRisky={250}
-          centralScoreDate="2023-08-31T00:00:00-05:00"
-          numberInternalBlackberries={9}
-          maximumNumberInstallmentsArrears={3}
+          centralScoreRisky={credit_behavior.core_risk_score}
+          centralScoreDate={String(credit_behavior.central_risk_score_date)}
+          numberInternalBlackberries={credit_behavior.number_of_internal_arrears}
+          maximumNumberInstallmentsArrears={credit_behavior.maximum_number_of_installments_in_arrears}
           isMobile={isMobile}
         />
       </Grid>
