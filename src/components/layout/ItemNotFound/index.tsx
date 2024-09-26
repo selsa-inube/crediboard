05238c76985dc26399d7a886ceb15edc40c48acd
Angelbt12 <angeldavidbaracaldo056@gmail.com>
2024-09-26 13:50:07 -0500

    fix: error screen setting

--- src/components/layout/ItemNotFound/index.tsx
+++ src/components/layout/ItemNotFound/index.tsx
@@ -23,7 +23,7 @@ function ItemNotFound(props: ItemNotFoundProps) {
       direction={smallScreen ? "column" : "row"}
       gap="30px"
       alignItems="center"
-      padding={smallScreen ? "35px" : "12px"}
+      padding={smallScreen ? "20px" : "30px"}
       justifyContent="center"
     >
       <StyledImage
