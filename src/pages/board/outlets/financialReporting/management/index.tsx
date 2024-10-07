fae487bead26ace54835fbcfd802351781283b79
Diego Pinilla <dpinilla@sistemasenlinea.com.co>
2024-10-07 11:10:10 -0500

    fix: adjust component

--- src/pages/board/outlets/financialReporting/management/index.tsx
+++ src/pages/board/outlets/financialReporting/management/index.tsx
@@ -1,22 +1,19 @@
 import { useState, useEffect, useCallback } from "react";
+import { LuPaperclip } from "react-icons/lu";
+import { MdOutlineSend } from "react-icons/md";
 import { Icon } from "@inubekit/icon";
-import { Stack, inube } from "@inube/design-system";
+import { Stack } from "@inubekit/stack";
 import { Textfield } from "@inubekit/textfield";
 
-import { LuPaperclip } from "react-icons/lu";
-import localforage from "localforage";
-import { MdOutlineSend } from "react-icons/md";
 import { Fieldset } from "@components/data/Fieldset";
 import { Message } from "@components/data/Message";
-import { updateActive } from "@mocks/utils/dataMock.service";
 import { TraceType } from "@services/types";
 import { ItemNotFound } from "@components/layout/ItemNotFound";
 import userNotFound from "@assets/images/ItemNotFound.png";
-import { traceObserver } from "../config";
+import { getTraceByCreditRequestId } from "@services/trace/getTraceByCreditRequestId";
 
+import { traceObserver, errorObserver } from "../config";
 import { ChatContent, SkeletonContainer, SkeletonLine } from "./styles";
-import { errorObserver } from "../config";
-import { getTraceByCreditRequestId } from "@src/services/trace/getTraceByCreditRequestId";
 
 interface IManagementProps {
   id: string;
@@ -46,19 +43,19 @@ export const Management = (props: IManagementProps) => {
     }, 5000);
 
     try {
-      const data = await getTraceByCreditRequestId(id);
-
-      clearTimeout(timer);
-
-      if (!data || (Array.isArray(data) && data.length === 0)) {
-        errorObserver.notify({
+      const data = await getTraceByCreditRequestId("c6a6173d-01bc-4d79-b5cc-9a6132f47347").catch(
+        () => {
+          errorObserver.notify({
           id: "Management",
           message: "Error al obtener los datos de gestión.",
         });
         setError("No se encontraron datos.");
-      } else if (data instanceof Error) {
-        setError("Error al obtener los datos de gestión.");
-      } else {
+        }
+      );
+
+      clearTimeout(timer);
+
+      if (data || (Array.isArray(data) && data.length > 0)) {
         const flattenedData: TraceType[] = Array.isArray(data[0])
           ? ((data as TraceType[]).flat() as TraceType[])
           : (data as TraceType[]);
@@ -108,24 +105,6 @@ export const Management = (props: IManagementProps) => {
       setTraces(updatedTraces);
 
       try {
-        await localforage.setItem("trace", updatedTraces);
-        await updateActive({
-          key: "traceId",
-          nameDB: "trace",
-          identifier: newTrace.traceId,
-          editData: {
-            traceId: newTrace.traceId,
-            traceValue: newTrace.traceValue,
-            creditRequestId: newTrace.creditRequestId,
-            useCase: newTrace.useCase ?? "",
-            userId: newTrace.userId,
-            excecutionDate: newTrace.excecutionDate as string,
-            justification: newTrace.justification ?? "",
-            decisionTakenByUser: newTrace.decisionTakenByUser ?? "",
-            traceType: newTrace.traceType ?? "",
-            readNovelty: newTrace.readNovelty ?? "",
-          },
-        });
         setNewMessage("");
       } catch (err) {
         console.error("Error al guardar el mensaje:", err);
@@ -175,7 +154,7 @@ export const Management = (props: IManagementProps) => {
                 ))}
           </ChatContent>
           <form>
-            <Stack alignItems="center" direction="row" gap={inube.spacing.s150}>
+            <Stack alignItems="center" direction="row" gap="4px">
               <Icon
                 appearance="primary"
                 cursorHover
