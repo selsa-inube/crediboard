088947feb6c42cc26765be5a393037c553b0467d
Angelbt12 <angeldavidbaracaldo056@gmail.com>
2024-10-08 10:34:01 -0500

    fix: modal settings

--- src/components/modals/AddSeriesModal/stories/props.ts
+++ src/components/modals/AddSeriesModal/stories/props.ts
@@ -23,17 +23,6 @@ export const props: Partial<ArgTypes<AddSeriesModalProps>> = {
     description: "ID del portal donde se renderiza el modal",
     defaultValue: "portal",
   },
-  formValues: {
-    control: {
-      type: "object",
-    },
-    description: "Valores del formulario",
-    defaultValue: {
-      field1: "",
-      field2: "3122638128",
-      field3: "3122638128",
-    },
-  },
   handleClose: {
     action: "handleClose",
     description: "Función para manejar el cierre del modal",
