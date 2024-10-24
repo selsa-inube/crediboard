38521b71e3f84f3e4f030f4547442171ace7e097
Angelbt12 <angeldavidbaracaldo056@gmail.com>
2024-10-24 15:50:48 -0500

    fix: bug settings

--- src/components/data/Fieldset/index.tsx
+++ src/components/data/Fieldset/index.tsx
@@ -1,39 +1,55 @@
+
+import { useState } from "react";
 import { MdAdd } from "react-icons/md";
 import { useMediaQuery } from "@inubekit/hooks";
 import { Text } from "@inubekit/text";
 import { Stack } from "@inubekit/stack";
 import { Button } from "@inubekit/button";
+
 import { StyledContainerFieldset } from "./styles";
 
-interface IPtionsButton {
+interface IOptionsButton {
   title: string;
   onClick?: () => void;
 }
 
 interface IFieldsetProps {
-  title: string;
   children: JSX.Element | JSX.Element[];
+  title?: string;
   aspectRatio?: string;
   heightFieldset?: string;
   descriptionTitle?: string;
-  activeButton?: IPtionsButton;
+  activeButton?: IOptionsButton;
   hasTable?: boolean;
   hasOverflow?: boolean;
+  slim?: boolean;
+  isMobile?: boolean;
+  isClickable?: boolean;
 }
 
 export const Fieldset = (props: IFieldsetProps) => {
   const {
     children,
     title,
-    heightFieldset = "auto",
+    heightFieldset,
     aspectRatio,
     descriptionTitle,
     activeButton,
     hasTable = false,
     hasOverflow,
+    isClickable,
   } = props;
 
   const isMobile = useMediaQuery("(max-width:880px)");
+
+  const [isSelected, setIsSelected] = useState(false);
+
+  const handleOnClick = () => {
+    if (isClickable) {
+      setIsSelected(!isSelected);
+    }
+    return;
+  };
 
   return (
     <Stack
@@ -49,7 +65,7 @@ export const Fieldset = (props: IFieldsetProps) => {
             appearance="gray"
             size={isMobile ? "medium" : "large"}
           >
-            {`${title} `}
+            {title}
           </Text>
           {descriptionTitle && (
             <Text type="title" ellipsis size={isMobile ? "medium" : "large"}>
@@ -72,11 +88,15 @@ export const Fieldset = (props: IFieldsetProps) => {
       <StyledContainerFieldset
         $aspectRatio={aspectRatio}
         $isMobile={isMobile}
-        $hasTable={hasTable}
         $hasOverflow={hasOverflow}
+        $hasTable={hasTable}
+        onClick={handleOnClick}
+        $isSelected={isSelected}
       >
         {children}
       </StyledContainerFieldset>
     </Stack>
   );
 };
+
+
