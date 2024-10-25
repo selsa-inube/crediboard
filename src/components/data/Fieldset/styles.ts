a7933f352ee851c41415da876b15b563ec022c0b
Angelbt12 <angeldavidbaracaldo056@gmail.com>
2024-10-25 09:01:02 -0500

    fix: simple settings

--- src/components/data/Fieldset/styles.ts
+++ src/components/data/Fieldset/styles.ts
@@ -10,15 +10,14 @@ interface IStyledContainerFieldset {
 }
 
 export const StyledContainerFieldset = styled.div<IStyledContainerFieldset>`
+  overflow-y: ${({ $hasOverflow }) => ($hasOverflow ? "visible" : "auto")};
   box-sizing: border-box;
-  overflow-x: hidden;
   border-radius: 8px;
   border: 1px solid;
-  padding-top: ${({ $isMobile }) => !$isMobile && "16px"};
-  padding-bottom: ${({ $isMobile }) => !$isMobile && "16px"};
-  padding-right: ${({ $isMobile }) => !$isMobile && "8px"};
-  padding-left: ${({ $isMobile }) => !$isMobile && "8px"};
-  overflow-x: ${({ $hasOverflow }) => ($hasOverflow ? "visible" : "auto")};
+  padding-top: ${({ $hasTable }) => !$hasTable && "16px"};
+  padding-bottom: ${({ $hasTable }) => !$hasTable && "16px"};
+  padding-right: ${({ $hasTable }) => !$hasTable && "8px"};
+  padding-left: ${({ $hasTable }) => !$hasTable && "8px"};
   aspect-ratio: ${({ $aspectRatio }) => $aspectRatio};
   background-color: ${({ theme, $isSelected }) =>
     !$isSelected
@@ -42,7 +41,7 @@ export const StyledContainerFieldset = styled.div<IStyledContainerFieldset>`
     !$isMobile &&
     `
     &::-webkit-scrollbar {
-      width: 8px; 
+      width: 16px; 
       border-radius: 8px;
     }
 
