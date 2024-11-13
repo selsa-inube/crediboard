ff7199367837d45b4f55e7afc789d6cae8f00a53
Brandonz07 <brodriguez@sistemasenlinea.com.co>
2024-11-13 10:23:37 -0500

    fix: add modal in loanamount

--- src/pages/add-prospect/steps/loanAmount/index.tsx
+++ src/pages/add-prospect/steps/loanAmount/index.tsx
@@ -9,8 +9,13 @@ import { Select } from "@inubekit/select";
 import { Icon } from "@inubekit/icon";
 import { inube } from "@inubekit/foundations";
 import { useMediaQuery } from "@inubekit/hooks";
-  
+
 import { Fieldset } from "@components/data/Fieldset";
+import { CreditLimit } from "@components/modals/CreditLimit";
+import { PaymentCapacity } from "@components/modals/PaymentCapacityModal";
+import { ReciprocityModal } from "@components/modals/ReciprocityModal";
+import { ScoreModal } from "@components/modals/FrcModal";
+
 import { currencyFormat } from "@utils/formatData/currency";
 import { get } from "@mocks/utils/dataMock.service";
 import { IPaymentChannel } from "@services/types";
@@ -27,6 +32,8 @@ export function LoanAmount(props: ILoanAmountProps) {
   const [requestValue, setRequestValue] = useState<IPaymentChannel[]>();
   const [form, setForm] = useState({ paymentPlan: "" });
   const [inputValue, setInputValue] = useState("");
+  const [openModal, setOpenModal] = useState<string | null>(null);
+  const [creditModal, setCreditModal] = useState(false);
 
   useEffect(() => {
     get("mockRequest_value")
@@ -70,7 +77,13 @@ export function LoanAmount(props: ILoanAmountProps) {
             <Text size="small" appearance="gray" weight="normal">
               {dataAmount.availableQuota}
             </Text>
-            <Icon icon={<MdInfoOutline />} appearance="primary" size="16px" />
+            <Icon
+              icon={<MdInfoOutline />}
+              appearance="primary"
+              size="16px"
+              onClick={() => setCreditModal(true)}
+              cursorHover={true}
+            />
           </Stack>
         </Stack>
         <Divider dashed />
@@ -123,6 +136,69 @@ export function LoanAmount(props: ILoanAmountProps) {
           />
         </Stack>
       </Stack>
+      {creditModal ? (
+        <CreditLimit
+          handleClose={() => setCreditModal(false)}
+          title="Origen de cupo"
+          portalId="portal"
+          maxPaymentCapacity={50000000}
+          maxReciprocity={40000000}
+          maxDebtFRC={45000000}
+          assignedLimit={0}
+          currentPortfolio={10000000}
+          maxUsableLimit={20000000}
+          availableLimitWithoutGuarantee={15000000}
+          onOpenPaymentCapacityModal={() => setOpenModal("paymentCapacity")}
+          onOpenReciprocityModal={() => setOpenModal("reciprocityModal")}
+          onOpenFrcModal={() => setOpenModal("scoreModal")}
+        />
+      ) : (
+        <></>
+      )}
+      {openModal === "paymentCapacity" ? (
+        <PaymentCapacity
+          title="Cupo máx. capacidad de pago"
+          portalId="portal"
+          handleClose={() => setOpenModal(null)}
+          reportedIncomeSources={2000000}
+          reportedFinancialObligations={6789000}
+          subsistenceReserve={2000000}
+          availableForNewCommitments={5000000}
+          maxVacationTerm={12}
+          maxAmount={1000000}
+          iconVisible={true}
+        />
+      ) : (
+        <></>
+      )}
+      {openModal === "reciprocityModal" ? (
+        <ReciprocityModal
+          portalId="portal"
+          handleClose={() => setOpenModal(null)}
+          balanceOfContributions={40000000}
+          accordingToRegulation={2}
+          assignedQuota={1000000}
+        />
+      ) : (
+        <></>
+      )}
+      {openModal === "scoreModal" ? (
+        <ScoreModal
+          title="Score Details"
+          handleClose={() => setOpenModal(null)}
+          subTitle="Your Financial Score"
+          totalScore={150}
+          seniority={150}
+          centralRisk={50}
+          employmentStability={230}
+          maritalStatus={30}
+          economicActivity={118}
+          monthlyIncome={3000000}
+          maxIndebtedness={50000000}
+        />
+      ) : (
+        <></>
+      )}
     </Fieldset>
   );
 }
