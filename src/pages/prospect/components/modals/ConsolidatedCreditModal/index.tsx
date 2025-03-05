e35234d474e6dfd8647ba1a27a8671db3bcb3ce3
JeissonLeoenlinea <jbarajas@sistemasenlinea.com.co>
2025-03-05 10:01:40 -0500

    fix: reusing the base modal component

--- src/pages/prospect/components/modals/ConsolidatedCreditModal/index.tsx
+++ src/pages/prospect/components/modals/ConsolidatedCreditModal/index.tsx
@@ -1,29 +1,104 @@
-import { createPortal } from "react-dom";
-import { Blanket } from "@inubekit/blanket";
-import { validationMessages } from "@validations/validationMessages";
-import { ConsolidatedCreditsInterface } from "./interface";
+import { Button } from "@inubekit/button";
+import { Stack } from "@inubekit/stack";
+import { Text } from "@inubekit/text";
+import { Divider } from "@inubekit/divider";
+import { useMediaQuery } from "@inubekit/hooks";
+import { Grid } from "@inubekit/grid";
+
+import { currencyFormat } from "@utils/formatData/currency";
+import { InvestmentCreditCard } from "@pages/addProspect/components/InvestmentCreditCard";
+import { BaseModal } from "@components/modals/baseModal";
+import { mockConsolidatedCreditModal } from "@mocks/add-prospect/consolidated-credit-modal/consolidatedcreditmodal.mock";
+
+import { ScrollableContainer } from "./styles";
+import { ModalConfig } from "./config";
 
 export interface ConsolidatedCreditsProps {
   handleClose: () => void;
-  portalId?: string;
   loading?: boolean;
 }
 
-export const ConsolidatedCredits = (props: ConsolidatedCreditsProps) => {
-  const { portalId, handleClose, loading = false } = props;
+export function ConsolidatedCredits(props: ConsolidatedCreditsProps) {
+  const { loading, handleClose } = props;
+  const isMobile = useMediaQuery("(max-width:880px)");
+  const data = mockConsolidatedCreditModal[0];
 
-  const node = document.getElementById(portalId ?? "portal");
-  if (!node) {
-    throw new Error(validationMessages.errorNodo);
-  }
-
-  return createPortal(
-    <Blanket>
-      <ConsolidatedCreditsInterface
-        loading={loading}
-        handleClose={handleClose}
-      />
-    </Blanket>,
-    node
+  return (
+    <BaseModal
+      title={ModalConfig.title}
+      nextButton={ModalConfig.keep}
+      disabledNext={true}
+      handleNext={handleClose}
+      width={isMobile ? "300px" : "644px"}
+      height={isMobile ? "auto" : "688px"}
+      handleBack={handleClose}
+      finalDivider={true}
+      portalId="portal"
+      backButton={ModalConfig.close}
+    >
+      <Stack direction="column" gap="24px">
+        <Stack
+          direction={isMobile ? "column" : "row"}
+          alignItems="center"
+          justifyContent={isMobile ? "center" : "space-between"}
+          gap={isMobile ? "10px" : "0px"}
+        >
+          <Stack direction="column">
+            <Text
+              appearance="primary"
+              weight="bold"
+              type="headline"
+              size="large"
+            >
+              $
+              {loading
+                ? ModalConfig.loading
+                : currencyFormat(data.collectedValue, false)}
+            </Text>
+            <Text type="body" appearance="gray" size="small" textAlign="center">
+              {ModalConfig.collectedValue}
+            </Text>
+          </Stack>
+          <Button
+            onClick={() => {}}
+            variant="outlined"
+            appearance="primary"
+            spacing="wide"
+            fullwidth={isMobile}
+          >
+            {ModalConfig.edit}
+          </Button>
+        </Stack>
+        <Divider dashed />
+        <ScrollableContainer>
+          <Stack
+            direction="column"
+            gap="24px"
+            height={isMobile ? "auto" : "420px"}
+          >
+            <Text type="body" appearance="gray" size="small" weight="bold">
+              {ModalConfig.selectedText}
+            </Text>
+            <Grid
+              autoRows="auto"
+              templateColumns={isMobile ? "1fr" : "repeat(2, 1fr)"}
+              gap="16px"
+              width="0%"
+            >
+              {data.investments.map((item, index) => (
+                <InvestmentCreditCard
+                  key={index}
+                  code={item.code}
+                  codeValue={item.codeValue}
+                  expired={item.expired}
+                  expiredValue={item.expiredValue}
+                  title={ModalConfig.creditInvestment}
+                />
+              ))}
+            </Grid>
+          </Stack>
+        </ScrollableContainer>
+      </Stack>
+    </BaseModal>
   );
-};
+}
