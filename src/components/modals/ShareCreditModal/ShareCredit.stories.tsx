879de665ccbcab0cfa5c30e7f9b3cc4c9500c0ad
Brandonz07 <brodriguez@sistemasenlinea.com.co>
2025-03-05 21:07:18 -0500

    fix: use basemodal in share

--- src/components/modals/ShareCreditModal/ShareCredit.stories.tsx
+++ src/components/modals/ShareCreditModal/ShareCredit.stories.tsx
@@ -25,7 +25,6 @@ export const Default: Story = (args: IShareCreditModalProps) => {
 };
 
 Default.args = {
-  portalId: "portal",
   isMobile: false,
 };
 
--- src/components/modals/ShareCreditModal/index.tsx
+++ src/components/modals/ShareCreditModal/index.tsx
@@ -1,33 +1,22 @@
-import { createPortal } from "react-dom";
 import { Formik } from "formik";
 import * as Yup from "yup";
-import { Blanket } from "@inubekit/blanket";
+import { MdInfoOutline } from "react-icons/md";
 import { Stack } from "@inubekit/stack";
 import { Text } from "@inubekit/text";
 import { Icon } from "@inubekit/icon";
-import { MdClear, MdInfoOutline } from "react-icons/md";
-import { Button } from "@inubekit/button";
-import { Divider } from "@inubekit/divider";
+import { Textfield } from "@inubekit/textfield";
 
-import { validationMessages } from "@validations/validationMessages";
+import { BaseModal } from "@components/modals/baseModal";
 
 import { dataSharModal } from "./config";
-import { StyledContainer, StyledContainerClose } from "./styles";
-import { Textfield } from "@inubekit/textfield";
 
 export interface IShareCreditModalProps {
   handleClose: () => void;
   isMobile: boolean;
-  portalId?: string;
 }
 
 export function ShareCreditModal(props: IShareCreditModalProps) {
-  const { handleClose, isMobile, portalId = "portal" } = props;
-
-  const node = document.getElementById(portalId ?? "portal");
-  if (!node) {
-    throw new Error(validationMessages.errorNodo);
-  }
+  const { handleClose, isMobile } = props;
 
   const initialValues = {
     name: "",
@@ -43,128 +32,95 @@ export function ShareCreditModal(props: IShareCreditModalProps) {
     share: Yup.boolean(),
   });
 
-  return createPortal(
+  return (
     <Formik
       initialValues={initialValues}
       validationSchema={validationSchema}
       onSubmit={() => {}}
     >
       {(formik) => (
-        <Blanket>
-          <StyledContainer>
-            <Stack
-              direction="column"
-              padding="24px"
-              gap="24px"
-              width={isMobile ? "287px" : "402px"}
-            >
-              <Stack justifyContent="space-between" alignItems="center">
-                <Text size="small" type="headline">
-                  {dataSharModal.title}
+        <BaseModal
+          title={dataSharModal.title}
+          nextButton={dataSharModal.share}
+          backButton={dataSharModal.cancel}
+          handleNext={() => {
+            formik.submitForm();
+            handleClose();
+          }}
+          handleBack={handleClose}
+          disabledNext={!formik.dirty || !formik.isValid}
+          width={isMobile ? "287px" : "402px"}
+        >
+          <Stack direction="column" gap="16px">
+            <Stack direction="column">
+              <Stack gap="4px">
+                <Text type="label" weight="bold" size="medium">
+                  {dataSharModal.name}
                 </Text>
-                <StyledContainerClose onClick={handleClose}>
-                  <Stack alignItems="center" gap="8px">
-                    <Text type="body" size="large">
-                      {dataSharModal.close}
-                    </Text>
-                    <Icon
-                      icon={<MdClear />}
-                      size="24px"
-                      cursorHover
-                      appearance="dark"
-                    />
-                  </Stack>
-                </StyledContainerClose>
+                <Text type="body" size="small" appearance="danger">
+                  {dataSharModal.required}
+                </Text>
               </Stack>
-              <Divider />
-              <Stack direction="column" gap="16px">
-                <Stack direction="column">
-                  <Stack gap="4px">
-                    <Text type="label" weight="bold" size="medium">
-                      {dataSharModal.name}
-                    </Text>
-                    <Text type="body" size="small" appearance="danger">
-                      {dataSharModal.required}
-                    </Text>
-                  </Stack>
-                  <Textfield
-                    name="name"
-                    id="name"
-                    placeholder={dataSharModal.placeHolderName}
-                    value={formik.values.name}
-                    onChange={formik.handleChange}
-                    onBlur={formik.handleBlur}
-                    fullwidth={true}
-                  />
-                </Stack>
-                <Stack direction="column">
-                  <Stack gap="4px">
-                    <Text type="label" weight="bold" size="medium">
-                      {dataSharModal.email}
-                    </Text>
-                    <Text type="body" size="small" appearance="danger">
-                      {dataSharModal.required}
-                    </Text>
-                  </Stack>
-                  <Textfield
-                    name="email"
-                    id="email"
-                    placeholder={dataSharModal.placeHolderEmail}
-                    value={formik.values.email}
-                    onChange={formik.handleChange}
-                    onBlur={formik.handleBlur}
-                    fullwidth={true}
-                  />
-                </Stack>
-                <Stack direction="column">
-                  <Stack gap="4px">
-                    <Text type="label" weight="bold" size="medium">
-                      {dataSharModal.aditionalEmail}
-                    </Text>
-                  </Stack>
-                  <Stack direction="column" gap="4px">
-                    <Textfield
-                      name="aditionalEmail"
-                      id="aditionalEmail"
-                      placeholder={dataSharModal.placeHolderAditionalEmail}
-                      value={formik.values.aditionalEmail}
-                      onChange={formik.handleChange}
-                      onBlur={formik.handleBlur}
-                      fullwidth={true}
-                    />
-                    <Stack gap="4px">
-                      <Icon
-                        icon={<MdInfoOutline />}
-                        appearance="primary"
-                        size="14px"
-                      ></Icon>
-                      <Text type="label" size="small">
-                        {dataSharModal.optional}
-                      </Text>
-                    </Stack>
-                  </Stack>
-                </Stack>
+              <Textfield
+                name="name"
+                id="name"
+                placeholder={dataSharModal.placeHolderName}
+                value={formik.values.name}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                fullwidth={true}
+              />
+            </Stack>
+            <Stack direction="column">
+              <Stack gap="4px">
+                <Text type="label" weight="bold" size="medium">
+                  {dataSharModal.email}
+                </Text>
+                <Text type="body" size="small" appearance="danger">
+                  {dataSharModal.required}
+                </Text>
               </Stack>
-              <Stack justifyContent="end" gap="20px">
-                <Button
-                  onClick={handleClose}
-                  appearance="gray"
-                  variant="outlined"
-                >
-                  {dataSharModal.cancel}
-                </Button>
-                <Button
-                  onClick={formik.submitForm}
-                  disabled={!formik.dirty || !formik.isValid}
-                >
-                  {dataSharModal.share}
-                </Button>
+              <Textfield
+                name="email"
+                id="email"
+                placeholder={dataSharModal.placeHolderEmail}
+                value={formik.values.email}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                fullwidth={true}
+              />
+            </Stack>
+            <Stack direction="column">
+              <Stack gap="4px">
+                <Text type="label" weight="bold" size="medium">
+                  {dataSharModal.aditionalEmail}
+                </Text>
+              </Stack>
+              <Stack direction="column" gap="4px">
+                <Textfield
+                  name="aditionalEmail"
+                  id="aditionalEmail"
+                  placeholder={dataSharModal.placeHolderAditionalEmail}
+                  value={formik.values.aditionalEmail}
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                  fullwidth={true}
+                />
+                <Stack gap="4px">
+                  <Icon
+                    icon={<MdInfoOutline />}
+                    appearance="primary"
+                    size="14px"
+                  ></Icon>
+                  <Text type="label" size="small">
+                    {dataSharModal.optional}
+                  </Text>
+                </Stack>
               </Stack>
             </Stack>
-          </StyledContainer>
-        </Blanket>
+          </Stack>
+        </BaseModal>
       )}
-    </Formik>,
-    node
+    </Formik>
   );
 }
