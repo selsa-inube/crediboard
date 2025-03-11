7a13b1e31150394a0b95822c18de7ad363e695b5
Brandonz07 <brodriguez@sistemasenlinea.com.co>
2025-03-11 18:53:32 -0500

    fix: validation in pinned add identification number

--- src/pages/board/outlets/boardlayout/interface.tsx
+++ src/pages/board/outlets/boardlayout/interface.tsx
@@ -35,6 +35,7 @@ interface BoardLayoutProps {
   handleSelectCheckChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
   handlePinRequest: (
     requestId: string,
+    identificationNumber: string[],
     userWhoPinnnedId: string,
     isPinned: string
   ) => void;
