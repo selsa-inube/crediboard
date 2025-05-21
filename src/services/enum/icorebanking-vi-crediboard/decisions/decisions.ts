5e944e6506d39674969c4170e191204fb6e756f9
Brandonz07 <brodriguez@sistemasenlinea.com.co>
2025-05-21 16:29:29 -0500

    fix: adjust decision with enum

---
+++ src/services/enum/icorebanking-vi-crediboard/decisions/decisions.ts
@@ -0,0 +1,109 @@
+export const decisions = [
+  {
+    Code: "ANALISIS_RIESGO",
+    Value: "Análisis Riesgo",
+    Description: "Devolver la Solicitud para Validar Requisitos y Riesgo.",
+    I18nAttribute: "DM_ENUM_EDECISIONS_ANALISIS_RIESGO",
+  },
+  {
+    Code: "ANULAR_SOLICITUD",
+    Value: "Anular Solicitud",
+    Description: "Anular la Solicitud porque se crea erróneamente.",
+    I18nAttribute: "DM_ENUM_EDECISIONS_ANULAR_SOLICITUD",
+  },
+  {
+    Code: "APROBAR_SOLICITUD",
+    Value: "Aprobar Solicitud",
+    Description: "Aprobar la Solicitud.",
+    I18nAttribute: "DM_ENUM_EDECISIONS_APROBAR_SOLICITUD",
+  },
+  {
+    Code: "CANCELAR_SOLICITUD",
+    Value: "Cancelar Solicitud",
+    Description:
+      "Cancelar la Solicitud porque NO hay interés de continuar con el trámite.",
+    I18nAttribute: "DM_ENUM_EDECISIONS_CANCELAR_SOLICITUD",
+  },
+  {
+    Code: "CONFIRMACION_CLIENTE",
+    Value: "Confirmación Cliente",
+    Description: "El cliente lo piensa y confirma plan de crédito.",
+    I18nAttribute: "DM_ENUM_EDECISIONS_CONFIRMACION_CLIENTE",
+  },
+  {
+    Code: "DESEMBOLSO_DECLINADO",
+    Value: "Desembolso Declinado",
+    Description: "El desembolso NO se pudo realizar.",
+    I18nAttribute: "DM_ENUM_EDECISIONS_DESEMBOLSO_DECLINADO",
+  },
+  {
+    Code: "DESEMBOLSO_REALIZADO",
+    Value: "Desembolso Realizado",
+    Description: "El desembolso del crédito se produjo satisfactoriamente.",
+    I18nAttribute: "DM_ENUM_EDECISIONS_DESEMBOLSO_REALIZADO",
+  },
+  {
+    Code: "GESTION_COMERCIAL",
+    Value: "Gestión Comercial",
+    Description: "Devolver la solicitud para Gestión Comercial.",
+    I18nAttribute: "DM_ENUM_EDECISIONS_GESTION_COMERCIAL",
+  },
+  {
+    Code: "RECHAZAR_SOLICITUD",
+    Value: "Rechazar Solicitud",
+    Description: "Rechazar la solicitud porque NO resulta viable.",
+    I18nAttribute: "DM_ENUM_EDECISIONS_RECHAZAR_SOLICITUD",
+  },
+  {
+    Code: "SOPORTES_INVALIDOS",
+    Value: "Soportes Inválidos",
+    Description: "Los soportes y/o las garantías NO son válidos.",
+    I18nAttribute: "DM_ENUM_EDECISIONS_SOPORTES_INVALIDOS",
+  },
+  {
+    Code: "SOPORTES_POS_NO_RECIBIDOS",
+    Value: "Soportes Pos No Recibidos",
+    Description:
+      "Los documentos soporte NO pueden ser entregados por el solicicitante.",
+    I18nAttribute: "DM_ENUM_EDECISIONS_SOPORTES_POS_NO_RECIBIDOS",
+  },
+  {
+    Code: "SOPORTES_POS_RECIBIDOS",
+    Value: "Soportes Pos Recibidos",
+    Description:
+      "Los documentos soporte requeridos segun el destino del dinero.",
+    I18nAttribute: "DM_ENUM_EDECISIONS_SOPORTES_POS_RECIBIDOS",
+  },
+  {
+    Code: "SOPORTES_VALIDOS",
+    Value: "Soportes Válidos",
+    Description: "Los soportes y las garantías son Válidos.",
+    I18nAttribute: "DM_ENUM_EDECISIONS_SOPORTES_VALIDOS",
+  },
+  {
+    Code: "VIABILIZAR_SOLICITUD",
+    Value: "Viabilizar Solicitud",
+    Description: "La solicitud es viable.",
+    I18nAttribute: "DM_ENUM_EDECISIONS_VIABILIZAR_SOLICITUD",
+  },
+  {
+    Code: "PREVIABILIZAR_SOLICITUD",
+    Value: "Previabilizar Solicitud",
+    Description: "La solicitud es viable.",
+    I18nAttribute: "DM_ENUM_EDECISIONS_PREVIABILIZAR_SOLICITUD",
+  },
+  {
+    Code: "CREAR_OBLIGACIONES_DE_CARTERA",
+    Value: "Crear Obligaciones de Cartera",
+    Description:
+      "Las obligaciones deben ser creadas usando la Api prevista para ello.",
+    I18nAttribute: "DM_ENUM_EDECISIONS_CREAR_OBLIGACIONES_DE_CARTERA",
+  },
+  {
+    Code: "DECLINAR_OBLIGACIONES_DE_CARTERA",
+    Value: "Declinar Obligaciones de Cartera",
+    Description:
+      "Debido a problemas con la interfaz de cartera NO es posible generar las obligaciones de cartera.",
+    I18nAttribute: "DM_ENUM_EDECISIONS_DECLINAR_OBLIGACIONES_DE_CARTERA",
+  },
+];
